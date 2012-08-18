a9c35230ee419eae67a3d4f9a062033ef6f3539d
anshuk <anshukk@gmail.com>
2012-08-18 09:48:36 +0530

    checking cwd

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-echo "Hellow World";die;
+echo "Hellow World,".get_cwd();
+echo phpinfo();
+
+die;
 require 'lib/klein.php';
 respond('gizurcloud/gizurrest/*', function () {
     $response->render('./api/index.php');
