0eaa545335176165c8af9f9c2e1a792a2568ff8d
anshuk <anshukk@gmail.com>
2012-08-18 10:34:50 +0530

    Config changes in vtiger / api / portal

--- applications/clab/trailer-app-portal/index.php
+++ applications/clab/trailer-app-portal/index.php
@@ -9,6 +9,6 @@ defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
-define(VT_REST_URL,'http://192.168.1.219/gizurcloud/api/index.php/api/');
+define(VT_REST_URL,'http://gizurtrailerapp-env.elasticbeanstalk.com/api/index.php/api/');
 require_once($yii);
 Yii::createWebApplication($config)->run();
