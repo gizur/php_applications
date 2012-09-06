c3481a2170356630c48f2549694edaf10f9e82a2
anshuk <anshukk@gmail.com>
2012-09-06 17:42:19 +0530

    gizur admin update

--- applications/gizur-admin/js/models/accounts.js
+++ applications/gizur-admin/js/models/accounts.js
@@ -3,7 +3,7 @@ define([
   'Backbone'
 ], function(_, Backbone) {
   var accountsModel = Backbone.Model.extend({
-     url: 'http://gizurtrailerapp-env.elasticbeanstalk.com/applications/index.php/api/User/',
+     url: 'http://gizurtrailerapp-env.elasticbeanstalk.com/api/index.php/api/User/',
   });
   return accountsModel;
 
