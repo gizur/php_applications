349e50357afa743bdf998281a2a842ed145bc646
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2012-09-24 09:50:39 +0200

    Updated SQL

--- instance-configuration/DynamoDB-example.sh
+++ instance-configuration/DynamoDB-example.sh
@@ -123,7 +123,7 @@ function createUser($mdb2, $username, $password) {
 
 
     $query = <<<EOT
-        CREATE DATABASE IF NOT EXISTS `$username`;
+       CREATE USER '$username'@'%' IDENTIFIED BY '$password';
 EOT;
 
     // Execute the query
