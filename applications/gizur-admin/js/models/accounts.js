beee835e581f336f95021766d944d7c76c479b0f
anshuk <anshukk@gmail.com>
2012-09-24 10:50:59 +0530

    setting reset form on create account

--- applications/gizur-admin/js/models/accounts.js
+++ applications/gizur-admin/js/models/accounts.js
@@ -3,7 +3,6 @@ define([
   'Backbone'
 ], function(_, Backbone) {
   var accountsModel = Backbone.Model.extend({
- //    url: 'http://localhost/gizurcloud/api/index.php/api/User/',
      url: RESTServerURL + 'User/',
   });
   return accountsModel;
