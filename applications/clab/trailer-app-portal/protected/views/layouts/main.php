7080a03fd284490853ba4a744fea745481d1387b
Anil Singh <gizur-ess-anil@gizur.com>
2012-09-29 12:38:07 +0530

    Update main css

--- applications/clab/trailer-app-portal/protected/views/layouts/main.php
+++ applications/clab/trailer-app-portal/protected/views/layouts/main.php
@@ -1,3 +1,4 @@
+cae19094815b922326f884215c12347f:Z5w1ATlivzfaYL2qoW7GSNcbuwyHkF2U 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
