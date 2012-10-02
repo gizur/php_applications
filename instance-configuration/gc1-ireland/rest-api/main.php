b688ad5030bf0469eff08ad3ea708c743eae6faf
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2012-10-02 20:44:52 +0200

    Updated memcahced server

--- instance-configuration/gc1-ireland/rest-api/main.php
+++ instance-configuration/gc1-ireland/rest-api/main.php
@@ -41,7 +41,7 @@ return array(
                     'class'=>'CMemCache',
                     'servers'=>array(
                         array(
-                            'host'=>'gizurcloud.i4vamf.0001.euw1.cache.amazonaws.com',
+                            'host'=>'memcached.int.gizur.com',
                             'port'=>11211,
                             'weight'=>100,
                         ),
