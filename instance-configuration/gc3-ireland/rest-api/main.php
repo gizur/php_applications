eac3fd3e3ffe8360e7e90fc98756843c681b6c4b
Anshuk <gizur-ess-anshuk@gizur.com>
2012-10-11 11:59:33 +0530

    removing memcache

--- instance-configuration/gc3-ireland/rest-api/main.php
+++ instance-configuration/gc3-ireland/rest-api/main.php
@@ -41,7 +41,7 @@ return array(
                     'class'=>'CMemCache',
                     'servers'=>array(
                         array(
-                            'host'=>'gizurcloud-1c.i4vamf.0001.euw1.cache.amazonaws.com',
+                            'host'=>'localhost',//gizurcloud-1c.i4vamf.0001.euw1.cache.amazonaws.com',
                             'port'=>11211,
                             'weight'=>100,
                         ),
