34f7177d3f27ddd48615b2e7bacf1f29f5d06d0a
Anshuk <gizur-ess-anshuk@gizur.com>
2012-10-12 14:47:53 +0530

    spell check folder

---
+++ applications/clab/trailer-app-portal/protected/extensions/language/en.php
@@ -0,0 +1,65 @@
+<?php
+global $langaugeArr;
+$langaugeArr = array (
+'Trailer App Portal' => 'Trailer App Portal',
+'Date' => 'Date',
+'Time' => 'Time',
+'Account' => 'Account',
+'Contact' => 'Contact',
+'Damage Reported' => 'Damage Reported',
+'In operation' => 'In operation',
+'Damaged' => 'Damaged',
+'Ticket ID' => 'Ticket ID',
+'Trailer ID' => 'Trailer ID',
+'Status of damage' => 'Status of damage',
+'Mark damage repaired' => 'Mark damage repaired',
+'Pictures' => 'Pictures',
+'Driver caused damage' => 'Driver caused damage',
+'Type of damage' => 'Type of damage',
+'Position on trailer for damage' => 'Position on trailer for damage',
+'Upload Pictures' => 'Upload Pictures',
+'Create Date' => 'Create Date',
+'Ticket Category' => 'Ticket Category',
+'Location for damage report' => 'Location for damage report',
+/*  For Mobile app  */
+'Report Damage' => 'Report Damage',
+'Reset' => 'Reset',
+'Submit' => 'Submit',
+'Back' => 'Back',
+'Cancel' => 'Cancel' ,
+'Survey' => 'Survey',
+'Trailer type' => 'Trailer type',
+'Own' => 'Own',
+'Rented' => 'Rented' ,
+'ID' => 'ID',
+'Place' => 'Place',
+'Sealed' => 'Sealed',
+'Yes' => 'Yes',
+'No' =>  'No',
+'Plates' => 'Plates',
+'Straps' => 'Straps',
+'Reported Damages' => 'Reported Damages',
+'Report a new damage' => 'Report a new damage',
+'Type' => 'Type',
+'Position' => 'Position',
+'Position on trailer' => 'Position on trailer',
+/*  Others  */
+'Total rows' => 'Total rows',
+'Rows' => 'Rows', 
+'Create new Trouble ticket' => 'Create new Trouble ticket',
+'Trouble ticket' => 'Trouble ticket',
+'Trouble ticket List' => 'Trouble ticket List',
+'Login' => 'Login',
+'Logout' => 'Logout',
+'About' => 'About',
+'Welcome' => 'Welcome',
+'Status of damage' => 'Status of damage',
+'Damage' => 'Damage'
+
+);
+function getTranslatedString($str)
+{
+	global $langaugeArr;
+	return (isset($langaugeArr[$str]))?$langaugeArr[$str]:$str;
+ }
+?>
