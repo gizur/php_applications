e83367c0163d76012256b04a0309d9f920aa75b2
Anshuk <gizur-ess-anshuk@gizur.com>
2012-10-15 12:10:22 +0530

    correcting config

--- instance-configuration/gc1-ireland/rest-api/main.php
+++ instance-configuration/gc1-ireland/rest-api/main.php
@@ -41,7 +41,7 @@ return array(
                     'class'=>'CMemCache',
                     'servers'=>array(
                         array(
-                            'host'=>'memcached.int.gizur.com',
+                            'host'=>'localhost',//'memcached.int.gizur.com',
                             'port'=>11211,
                             'weight'=>100,
                         ),
@@ -110,7 +110,7 @@ return array(
 	'params'=>array(
 		// this is used in contact page
                 'vtRestUrl'   => 'http://api.gizur.com/lib/vtiger-5.4.0/webservice.php',
-                'awsS3Bucket' => 'gizurcloud',
+                'awsS3Bucket' => 'gizurcloud-clab',
                 'awsDynamoDBTableName' => 'GIZUR_ACCOUNTS',
                 'awsSESFromEmailAddress' => 'noreply@gizur.com',
                 'acceptableTimestampError' => 60,
