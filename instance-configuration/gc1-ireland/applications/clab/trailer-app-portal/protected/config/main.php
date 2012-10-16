c93756c3d3d59ccc7c7b01e5ce3adff6d4ed08b4
Anshuk <gizur-ess-anshuk@gizur.com>
2012-10-16 15:10:21 +0530

    http and https url

--- instance-configuration/gc1-ireland/applications/clab/trailer-app-portal/protected/config/main.php
+++ instance-configuration/gc1-ireland/applications/clab/trailer-app-portal/protected/config/main.php
@@ -91,6 +91,7 @@ return array(
 	'params'=>array(
 		// this is used in contact page
 		'adminEmail'=>'webmaster@example.com',
+                'protocol'=>'https://',
 		'GIZURCLOUD_SECRET_KEY' => '50694086b18cd0.9497426050694086b18fa8.66729980',
 		'GIZURCLOUD_API_KEY' => 'GZCLD50694086B196F50694086B19E7',
 		'API_VERSION' => '0.1',
