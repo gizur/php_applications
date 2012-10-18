bb62eff69615b31ae1491f671fbc8978e3c88271
Anshuk <gizur-ess-anshuk@gizur.com>
2012-10-18 13:58:16 +0530

    Disabling Cache

--- api/protected/config/main.php
+++ api/protected/config/main.php
@@ -38,14 +38,14 @@ return array(
 			'allowAutoLogin'=>true,
 		),
                 'cache'=>array(
-                    'class'=>'CMemCache',
-                    'servers'=>array(
-                        array(
-                            'host'=>'localhost',//gizurcloud-1c.i4vamf.0001.euw1.cache.amazonaws.com',
-                            'port'=>11211,
-                            'weight'=>100,
-                        ),
-                    ),
+                    'class'=>'CDummyCache',
+                    //'servers'=>array(
+                    //    array(
+                    //        'host'=>'localhost',//gizurcloud-1c.i4vamf.0001.euw1.cache.amazonaws.com',
+                    //        'port'=>11211,
+                    //        'weight'=>100,
+                    //    ),
+                    //),
                 ),            
 		// uncomment the following to enable URLs in path-format
 		'urlManager'=>array(
