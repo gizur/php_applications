9e809e634a6b696dc2f9892426185ca78d4d6368
Anshuk <gizur-ess-anshuk@gizur.com>
2012-10-22 17:12:01 +0530

    Cron: shell_exec

--- api/protected/controllers/ApiController.php
+++ api/protected/controllers/ApiController.php
@@ -2317,7 +2317,7 @@ class ApiController extends Controller
                     
                     $filename = Yii::app()->params->vtCronPath . 'MailScanner.service';
                     $response = new stdClass();
-                    $response->result = shell_exec("/usr/lib/php -f $filename");
+                    $response->result = shell_exec("/usr/bin/php -f $filename");
                     
                     $this->_sendResponse(200, json_encode($response));
                 }
