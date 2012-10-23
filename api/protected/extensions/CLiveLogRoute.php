c578fc8f7e29bd10693e39c6a2cfab4e7ced5231
Anshuk <gizur-ess-anshuk@gizur.com>
2012-10-23 09:36:11 +0530

    Live / Centralized Logging

---
+++ api/protected/extensions/CLiveLogRoute.php
@@ -0,0 +1,74 @@
+<?php
+/**
+* LiveLogRoute class file.
+*
+* @author Anshuk Kumar <anshuk.kumar@essindia.co.in>
+* @link http://www.gizur.com/
+* @copyright Copyright &copy; 2008-2011 Gizur AB
+* @license none
+*/
+
+/**
+* CLiveLogRoute sends selected log messages to and external server.
+*
+* The target server addresses may be specified via {@link setServer server} property.
+*
+* @property string $server The URL to POST the log to.
+*
+* @author Anshuk Kumar <anshuk.kumar@essindia.co.in>
+* @version $Id$
+* @package system.logging
+*/
+class CLiveLogRoute extends CLogRoute
+{
+    /**
+    * @var string server address(url).
+    */
+    private $_server;
+    /**
+    * Sends log messages to specified server address.
+    * @param array $logs list of log messages
+    */
+    protected function processLogs($logs)
+    {
+        $message='';
+        foreach($logs as $log)
+        $message.=$this->formatLogMessage($log[0],$log[1],$log[2],$log[3]);
+        $message=wordwrap($message,70);
+        $this->postLog($this->getServer(), $message);
+    }
+
+    /**
+    * Sends the POST to server.
+    * @param string $server server url / address
+    * @param string $message POST content
+    */
+    protected function postLog($server, $message)
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL,            $server );
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+        curl_setopt($ch, CURLOPT_POST,           1 );
+        curl_setopt($ch, CURLOPT_POSTFIELDS,     $message ); 
+        curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Content-Type: text/plain')); 
+
+        $result=curl_exec ($ch);
+    }
+
+    /**
+    * @return string server url / address
+    */
+    public function getServer()
+    {
+        return $this->_server;
+    }
+
+    /**
+    * @param string $value server url / address
+    */
+    public function setServer($value)
+    {
+        $this->_server=$value;
+    }
+}
