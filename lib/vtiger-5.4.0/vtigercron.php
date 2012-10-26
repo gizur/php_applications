81795c700af492f3af59d4c2652eced942b6d642
Anshuk <gizur-ess-anshuk@gizur.com>
2012-10-26 16:45:51 +0530

    disabling display errors

--- lib/vtiger-5.4.0/vtigercron.php
+++ lib/vtiger-5.4.0/vtigercron.php
@@ -8,9 +8,6 @@
  * All Rights Reserved.
  ********************************************************************************/
 
-error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
-ini_set('display_errors',"1");
-
 /**
  * Start the cron services configured.
  */
