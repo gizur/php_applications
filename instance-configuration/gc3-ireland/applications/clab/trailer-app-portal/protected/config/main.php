82a3d47c7a2d6021149a0723ba62a0def508c13f
Anshuk <gizur-ess-anshuk@gizur.com>
2012-11-21 10:57:04 +0530

    adding logging level trace

--- instance-configuration/gc3-ireland/applications/clab/trailer-app-portal/protected/config/main.php
+++ instance-configuration/gc3-ireland/applications/clab/trailer-app-portal/protected/config/main.php
@@ -74,7 +74,7 @@ return array(
 			'routes'=>array(
 				array(
 					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
+					'levels'=>'trace, error, warning',
 				),
 				// uncomment the following to show log messages on web pages
 				/*
