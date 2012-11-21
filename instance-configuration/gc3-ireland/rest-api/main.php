70e3454601adbd1d95f89b2f062c5ca7b4e8dd98
Anshuk <gizur-ess-anshuk@gizur.com>
2012-11-21 12:05:18 +0530

    adding traces

--- instance-configuration/gc3-ireland/rest-api/main.php
+++ instance-configuration/gc3-ireland/rest-api/main.php
@@ -95,7 +95,7 @@ return array(
 			'routes'=>array(
 				array(
 					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
+					'levels'=>'trace, error, warning',
 				),
 				array(
 					'class'=>'CLiveLogRoute',
