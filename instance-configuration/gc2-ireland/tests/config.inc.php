e7f57d4e281169972f887775f4d4c6269ffefe68
Anshuk <gizur-ess-anshuk@gizur.com>
2012-11-30 12:03:28 +0530

    configuring vt wrapper

--- instance-configuration/gc2-ireland/tests/config.inc.php
+++ instance-configuration/gc2-ireland/tests/config.inc.php
@@ -4,7 +4,7 @@ class configuration {
 
     public function get() {
 	    return array(
-	       'url' => 'https://c2.gizur.com/api/index.php/api/',
+	       'url' => 'https://c2.gizur.com/api/',
 	       //'url' => 'https://phpapplications3-env-tk3itzr6av.elasticbeanstalk.com/api/index.php/api/',
 	       'GIZURCLOUD_API_KEY' => 'GZCLD50826A54755AB50826A5475624',
 	       'GIZURCLOUD_SECRET_KEY' => '50826a54755009.5822592450826a54755292.56509362',
