c92bfd327dbcbc3ec2f1719b69ec856a99393796
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2012-12-21 10:39:09 +0530

    Routing enabled and config rolled back to the dynamic version.

--- applications/cikab/bikeportal/PortalConfig.php
+++ applications/cikab/bikeportal/PortalConfig.php
@@ -15,7 +15,7 @@ global $Portal_Path;
 
 //This is the vtiger server path ie., the url to access the vtiger server in browser
 //Ex. i access my vtiger as http://mickie:90/vtiger/index.php so i will give as http://mickie:90/vtiger
-$Server_Path = "http://phpapplications-env-sixmtjkbzs.elasticbeanstalk.com/lib/vtiger-5.4.0";
+$Server_Path = "http://phpapplications-env-sixmtjkbzs.elasticbeanstalk.com/cikab/vtiger";
 
 //This is the customer portal path ie., url to access the customer portal in browser 
 //Ex. i access my portal as http://mickie:90/customerportal/login.php so i will give as http://mickie:90/customerportal
