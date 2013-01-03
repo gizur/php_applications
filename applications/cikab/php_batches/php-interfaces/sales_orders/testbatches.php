66431cb80ae4ad2ac4b962b15849585b11c49002
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-01-03 16:17:33 +0530

    test script added in php_batches.

--- applications/cikab/php_batches/php-interfaces/sales_orders/testbatches.php
+++ applications/cikab/php_batches/php-interfaces/sales_orders/testbatches.php
@@ -3,19 +3,19 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 $output = null;
 switch($_GET['action']){
-    case 'table':
-        $output = shell_exec('setup-tables.sh');
+    case 'setup-tables':
+        $output = shell_exec('./setup-tables.sh');
         break;
     case 'phpcronjob1':
-        $output = shell_exec('phpcronjob1.sh');
+        $output = shell_exec('./phpcronjob1.sh');
         break;
     case 'phpcronjob2':
-        $output = shell_exec('phpcronjob2.sh');
+        $output = shell_exec('./phpcronjob2.sh');
         break;
     case 'phpcronjob3':
-        $output = shell_exec('phpcronjob3.sh');
+        $output = shell_exec('./phpcronjob3.sh');
         break;
-    case 'phoinfo':
+    case 'phpinfo':
         phpinfo();
         break;
 }
