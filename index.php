4b54ac78285148f9768847c25c89d23f7499d684
Anshuk <gizur-ess-anshuk@gizur.com>
2013-01-04 10:37:24 +0530

    new logic for picklist

--- index.php
+++ index.php
@@ -1,28 +1,2 @@
 <?php
-require 'lib/klein.php';
-$baseURL = '/gizurcloud';
-
-respond($baseURL . '/api/[:trailing]', function ($request,$response) {
-    $response->query(array('/api/' . $request->trailing));
-    $response->render('./api/index.php');
-});
-
-respond($baseURL . '/[a:clientid]/trailer_app/[:trailing]', function ($request,$response) {
-    $response->query(array($request->trailing));
-    $response->render('./applications/' . $request->clientid . '/trailer_app_portal/index.php');
-});
-
-respond($baseURL . '/[a:clientid]/vtiger/index.php?[:trailing]', function($request, $response) {
-    $response->query(array(
-        $request->trailing, 
-        'clientid' => $request->clientid,
-    ));
-    $response->render('./lib/vtwrapper-index.php');
-});
-
-respond($baseURL . '/index.php', function($request, $response)) {    
-    echo "<pre></pre>";
-    print_r($request);
-}
-
-dispatch();
+echo "TESTING ADSSDFASD FASD -- PP";
