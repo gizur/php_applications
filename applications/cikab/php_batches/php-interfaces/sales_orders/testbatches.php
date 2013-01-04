6aaa423f8bc35f12fbc08bc3b0da7f963891b8b4
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-01-04 17:34:03 +0530

    Amazon SQS configured.

--- applications/cikab/php_batches/php-interfaces/sales_orders/testbatches.php
+++ applications/cikab/php_batches/php-interfaces/sales_orders/testbatches.php
@@ -32,22 +32,22 @@ if (isset($_GET['action'])) {
     }
     echo "<pre>$output</pre>";
 }
-include '../config.inc.php';
+/*include '../config.inc.php';
 include '../../../../../lib/aws-php-sdk/sdk.class.php';
 
 echo "<pre>";
-$sqs = new AmazonSQS();
+$sqs = new AmazonSQS();*/
 /*$_message = urlencode("I am here");
 $_response = $sqs->send_message($amazonqueue_config['_url'], $_message);
 if($_response->status == 200){
     echo "Successfull";
 }*/
-$_response1 = $sqs->receive_message($amazonqueue_config['_url']);
+/*$_response1 = $sqs->receive_message($amazonqueue_config['_url']);
 if($_response1->status == 200){
     print_r($_response1);
     if(empty($_response1->body->ReceiveMessageResult->Message))
         echo 'EMPTY';
     echo $msgObj = $_response1->body->ReceiveMessageResult->Message->MessageId;
     $sqs->delete_message($amazonqueue_config['_url'], $_response1->body->ReceiveMessageResult->Message->ReceiptHandle);
-}
+}*/
 ?>
