f39a66f97ca8922a21f3946bee2721ccc93f5ff1
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-01-15 22:29:57 +0530

    Error reporting oFF in CommonUtils

--- tests/dynamodb.php
+++ tests/dynamodb.php
@@ -10,15 +10,13 @@ $queue->use_credentials($dynamodb->credentials);
 $region = 'REGION_EU_W1';
 $table_name = 'VTIGER_TABDATA';
 //$dynamodb->set_region("AmazonDynamoDB::" . $region);
-
-
 // Prepare the data
-$post['id'] = $gizur_client_id;
-$post['tab_info_array'] = 'constructArray($result_array)';
-$post['tab_seq_array'] = 'constructArray($seq_array)';
-$post['tab_ownedby_array'] = 'constructArray($ownedby_array)';
-$post['action_id_array'] = 'constructSingleStringKeyAndValueArray($actionid_array)';
-$post['action_name_array'] = 'constructSingleStringValueArray($actionname_array)';
+$post['id'] = array(AmazonDynamoDB::TYPE_STRING => $gizur_client_id);
+$post['tab_info_array'] = array(AmazonDynamoDB::TYPE_STRING => 'constructArray($result_array)');
+$post['tab_seq_array'] = array(AmazonDynamoDB::TYPE_STRING => 'constructArray($seq_array)');
+$post['tab_ownedby_array'] = array(AmazonDynamoDB::TYPE_STRING => 'constructArray($ownedby_array)');
+$post['action_id_array'] = array(AmazonDynamoDB::TYPE_STRING => 'constructSingleStringKeyAndValueArray($actionid_array)');
+$post['action_name_array'] = array(AmazonDynamoDB::TYPE_STRING => 'constructSingleStringValueArray($actionname_array)');
 echo "In create_tab_data_file() $gizur_client_id";
 
 $dynamodb->batch($queue)->put_item(
