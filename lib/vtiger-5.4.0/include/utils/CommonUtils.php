223b9bbe17baa6120b84e7414904cbefd77c3876
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-01-16 13:02:53 +0530

    DynamoDB added in tabdata.php

--- lib/vtiger-5.4.0/include/utils/CommonUtils.php
+++ lib/vtiger-5.4.0/include/utils/CommonUtils.php
@@ -1666,11 +1666,11 @@ function create_tab_data_file() {
     $queue->use_credentials($dynamodb->credentials);
     // Prepare the data
     $post['id'] = array(AmazonDynamoDB::TYPE_STRING => $gizur_client_id);
-    $post['tab_info_array'] = array(AmazonDynamoDB::TYPE_STRING => serialize(constructArray($result_array)));
-    $post['tab_seq_array'] = array(AmazonDynamoDB::TYPE_STRING => serialize(constructArray($seq_array)));
-    $post['tab_ownedby_array'] = array(AmazonDynamoDB::TYPE_STRING => serialize(constructArray($ownedby_array)));
-    $post['action_id_array'] = array(AmazonDynamoDB::TYPE_STRING => serialize(constructSingleStringKeyAndValueArray($actionid_array)));
-    $post['action_name_array'] = array(AmazonDynamoDB::TYPE_STRING => serialize(constructSingleStringValueArray($actionname_array)));
+    $post['tab_info_array'] = array(AmazonDynamoDB::TYPE_STRING => json_encode(constructArray($result_array)));
+    $post['tab_seq_array'] = array(AmazonDynamoDB::TYPE_STRING => json_encode(constructArray($seq_array)));
+    $post['tab_ownedby_array'] = array(AmazonDynamoDB::TYPE_STRING => json_encode(constructArray($ownedby_array)));
+    $post['action_id_array'] = array(AmazonDynamoDB::TYPE_STRING => json_encode(constructSingleStringKeyAndValueArray($actionid_array)));
+    $post['action_name_array'] = array(AmazonDynamoDB::TYPE_STRING => json_encode(constructSingleStringValueArray($actionname_array)));
 
     $log->debug("In create_tab_data_file() $gizur_client_id");
     
