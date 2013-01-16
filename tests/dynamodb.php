7f96e9d2d1ef834a74587b328d824421792cdfe2
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-01-16 10:25:28 +0530

    dynamodb test file modified.

--- tests/dynamodb.php
+++ tests/dynamodb.php
@@ -7,36 +7,38 @@ $region = 'REGION_EU_W1';
 $table_name = 'VTIGER_TABDATA';
 $dynamodb->set_region(AmazonDynamoDB::REGION_EU_W1);
 
-$response = $dynamodb->query(array(
-    'TableName' => $table_name,
-    'HashKeyValue' => array(AmazonDynamoDB::TYPE_STRING => $gizur_client_id),
-    )
-);
-print_r($responses);
-/*
-$queue = new CFBatchRequest();
-$queue->use_credentials($dynamodb->credentials);
-// Prepare the data
-$post['id'] = array(AmazonDynamoDB::TYPE_STRING => $gizur_client_id);
-$post['tab_info_array'] = array(AmazonDynamoDB::TYPE_STRING => 'constructArray($result_array)');
-$post['tab_seq_array'] = array(AmazonDynamoDB::TYPE_STRING => 'constructArray($seq_array)');
-$post['tab_ownedby_array'] = array(AmazonDynamoDB::TYPE_STRING => 'constructArray($ownedby_array)');
-$post['action_id_array'] = array(AmazonDynamoDB::TYPE_STRING => 'constructSingleStringKeyAndValueArray($actionid_array)');
-$post['action_name_array'] = array(AmazonDynamoDB::TYPE_STRING => 'constructSingleStringValueArray($actionname_array)');
-echo "In create_tab_data_file() $gizur_client_id";
-
-$dynamodb->batch($queue)->put_item(
+$response = $dynamodb->get_item(
     array(
         'TableName' => $table_name,
-        'Item' => $post
+        'Key' => $dynamodb->attributes(array('HashKeyValue' => array(AmazonDynamoDB::TYPE_STRING => $gizur_client_id))),
+        'ConsistentRead' => 'true'
     )
 );
+print_r($response);
+/*
+  $queue = new CFBatchRequest();
+  $queue->use_credentials($dynamodb->credentials);
+  // Prepare the data
+  $post['id'] = array(AmazonDynamoDB::TYPE_STRING => $gizur_client_id);
+  $post['tab_info_array'] = array(AmazonDynamoDB::TYPE_STRING => 'constructArray($result_array)');
+  $post['tab_seq_array'] = array(AmazonDynamoDB::TYPE_STRING => 'constructArray($seq_array)');
+  $post['tab_ownedby_array'] = array(AmazonDynamoDB::TYPE_STRING => 'constructArray($ownedby_array)');
+  $post['action_id_array'] = array(AmazonDynamoDB::TYPE_STRING => 'constructSingleStringKeyAndValueArray($actionid_array)');
+  $post['action_name_array'] = array(AmazonDynamoDB::TYPE_STRING => 'constructSingleStringValueArray($actionname_array)');
+  echo "In create_tab_data_file() $gizur_client_id";
 
-$responses = $dynamodb->batch($queue)->send();
-*/
+  $dynamodb->batch($queue)->put_item(
+  array(
+  'TableName' => $table_name,
+  'Item' => $post
+  )
+  );
+
+  $responses = $dynamodb->batch($queue)->send();
+ */
 if ($responses->areOK()) {
     echo "The data has been added to the table." . PHP_EOL;
 } else {
-    print_r($responses);
+    print_r($response);
 }
 ?>
