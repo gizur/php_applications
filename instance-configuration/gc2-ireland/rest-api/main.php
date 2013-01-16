e459dafaeda041151c3b9847c162768595eb0fd5
Anshuk <gizur-ess-anshuk@gizur.com>
2013-01-16 11:07:31 +0530

    updating cache config for gc2

--- instance-configuration/gc2-ireland/rest-api/main.php
+++ instance-configuration/gc2-ireland/rest-api/main.php
@@ -38,15 +38,15 @@ return array(
 			'allowAutoLogin'=>true,
 		),
                 'cache'=>array(
-                      'class'=>'CDummyCache',
-//                    'class'=>'CMemCache',
-//                    'servers'=>array(
-//                        array(
-//                            'host'=>'localhost',
-//                            'port'=>11211,
-//                            'weight'=>100,
-//                        ),
-//                    ),
+//                      'class'=>'CDummyCache',
+                    'class'=>'CMemCache',
+                    'servers'=>array(
+                        array(
+                            'host'=>'gc2-memcache.oztphl.cfg.euw1.cache.amazonaws.com',
+                            'port'=>11211,
+                            'weight'=>100,
+                        ),
+                    ),
                 ),            
 		// uncomment the following to enable URLs in path-format
 		'urlManager'=>array(
