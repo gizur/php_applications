b889b5a2863bd743a0996827a9acd379aa1e6af0
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-01-23 13:02:56 +0530

    Added GizurSaaS.

--- applications/gizursaas/config/config.js
+++ applications/gizursaas/config/config.js
@@ -31,12 +31,12 @@ $(function() {
     
         $.get(_url,{},function(_data){
             var data = {
-                'email' : _data.result.id,
-                'api_key_1' : _data.result.apikey_1,
-                'api_key_2' : _data.result.apikey_2,
-                'secret_key_1' : _data.result.secretkey_2,
-                'secret_key_2' : _data.result.secretkey_1,
-                'old_email' : _data.result.id
+                'value_email' : _data.result.id,
+                'value_api_key_1' : _data.result.apikey_1,
+                'value_api_key_2' : _data.result.apikey_2,
+                'value_secret_key_1' : _data.result.secretkey_2,
+                'value_secret_key_2' : _data.result.secretkey_1,
+                'value_old_email' : _data.result.id
             };
             $.get('templates/home.tmp.html',{},function(html){
                 var output = Plates.bind(html, data);
