cb9429a0469a7127b8e51c01a15cc0f9f1c8f621
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-01-23 14:23:21 +0530

    Added GizurSaaS.

--- applications/gizursaas/config/config.js
+++ applications/gizursaas/config/config.js
@@ -36,9 +36,10 @@ $(function() {
                     $('#email').val(_data.result.id);
                     $('#api_key_1').val(_data.result.apikey_1);
                     $('#api_key_2').val(_data.result.apikey_2);
-                    $('#secret_key_1').val(_data.result.secretkey_1);
-                    $('#secret_key_2').val(_data.result.secretkey_2);
+                    $('#secret_key_1').text(_data.result.secretkey_1);
+                    $('#secret_key_2').text(_data.result.secretkey_2);
                     $('#old_email').val(_data.result.id);
+                    $('#client_id').val(_data.result.clientid);
                 });
             }else{
                 
