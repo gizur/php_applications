0a60adee83347ed9c1dfbf9c8ca1a0cb4b62750c
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-01-23 14:17:52 +0530

    Added GizurSaaS.

--- applications/gizursaas/config/config.js
+++ applications/gizursaas/config/config.js
@@ -29,27 +29,20 @@ $(function() {
         
         var _url = __rest_server_url + 'User/' + encodeURIComponent(__client_email);
     
-        var _data = {'result' : {'apikey_1':'apikey_1111111'}};
         $.get(_url,{},function(_data){
-            var data = {
-                'email' : _data.result.id,
-                'api_key_1' : _data.result.apikey_1,
-                'api_key_2' : _data.result.apikey_2,
-                'secret_key_1' : _data.result.secretkey_1,
-                'secret_key_2' : _data.result.secretkey_2,
-                'old_email' : _data.result.id
-            };
-            var map = Plates.Map();
-            map.where('id').is('email').use('email').as('value');
-            map.where('id').is('api_key_1').use('api_key_1').as('value');
-            map.where('id').is('api_key_2').use('api_key_2').as('value');
-            map.where('id').is('old_email').use('old_email').as('value');
-            map.where('id').is('client_id').use('client_id').as('value');
-            
-            $.get('templates/home.tmp.html',{},function(html){
-                var output = Plates.bind(html, data, map);
-                $('#container').empty().html(output);
-            });
+            if(_data.success){
+                $.get('templates/home.tmp.html',{},function(html){
+                    $('#container').empty().html(html);
+                    $('#email').val(_data.result.id);
+                    $('#api_key_1').val(_data.result.apikey_1);
+                    $('#api_key_2').val(_data.result.apikey_2);
+                    $('#secret_key_1').val(_data.result.secretkey_1);
+                    $('#secret_key_2').val(_data.result.secretkey_2);
+                    $('#old_email').val(_data.result.id);
+                });
+            }else{
+                
+            }
         });    
     });
     
@@ -69,3 +62,25 @@ $(function() {
     hasher.changed.add(parseHash); //parse hash changes
     hasher.init(); //start listening for hash changes
 });
+
+/*
+ * PLATES CODE
+    
+    var data = {
+        'email' : _data.result.id,
+        'api_key_1' : _data.result.apikey_1,
+        'api_key_2' : _data.result.apikey_2,
+        'secret_key_1' : _data.result.secretkey_1,
+        'secret_key_2' : _data.result.secretkey_2,
+        'old_email' : _data.result.id
+    };
+    var map = Plates.Map();
+    map.where('id').is('email').use('email').as('value');
+    map.where('id').is('api_key_1').use('api_key_1').as('value');
+    map.where('id').is('api_key_2').use('api_key_2').as('value');
+    map.where('id').is('old_email').use('old_email').as('value');
+    map.where('id').is('client_id').use('client_id').as('value');
+
+    var output = Plates.bind(html, data, map);
+                
+ */
