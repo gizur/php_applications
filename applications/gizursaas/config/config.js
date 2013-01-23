e1bd0ce7c0902ea06955e1bd524e5cf2f87b9f01
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-01-23 14:09:25 +0530

    Added GizurSaaS.

--- applications/gizursaas/config/config.js
+++ applications/gizursaas/config/config.js
@@ -35,8 +35,8 @@ $(function() {
                 'email' : _data.result.id,
                 'api_key_1' : _data.result.apikey_1,
                 'api_key_2' : _data.result.apikey_2,
-                'secret_key_1' : _data.result.secretkey_2,
-                'secret_key_2' : _data.result.secretkey_1,
+                'secret_key_1' : _data.result.secretkey_1,
+                'secret_key_2' : _data.result.secretkey_2,
                 'old_email' : _data.result.id
             };
             var map = Plates.Map();
