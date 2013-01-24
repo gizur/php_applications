6fb3f573efc57564d51693812be0b67f3617ef36
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-01-24 13:11:55 +0530

    GizurSaaS added.

--- applications/gizursaas/app/controller/AccountsController.js
+++ applications/gizursaas/app/controller/AccountsController.js
@@ -41,8 +41,8 @@ var AccountsController = Stapes.subclass({
                         .html('<button data-dismiss="alert" class="close" type="button">×</button>Key pair has been generated successfully.');
                         
                         //Assign values to the Account Object
-                        self.model.api_key_1 = _data.result.apikey_1;
-                        self.model.secret_key_1 = _data.result.secretkey_1;
+                        self.model.setApiKey1(_data.result.apikey_1);
+                        self.model.setSecretKey1(_data.result.secretkey_1);
                         //Map values to the page
                         self.model.emit('mapValues');
                         
