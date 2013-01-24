4cf3f1c03347ae2a91cda200638b6723bcd522d3
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-01-24 10:54:26 +0530

    GizurSaaS added.

--- applications/gizursaas/app/controller/AccountsController.js
+++ applications/gizursaas/app/controller/AccountsController.js
@@ -8,8 +8,8 @@ var AccountsController = Stapes.subclass({
         var $client_id = this.$el.find("#client_id");
         var $api_key_1 = this.$el.find("#api_key_1");
         var $api_key_2 = this.$el.find("#api_key_2");
-        var $secret_key_1 = $("#secret_key_1").text();
-        var $secret_key_2 = $("#secret_key_2").text();
+        var $secret_key_1 = $("#secret_key_1");
+        var $secret_key_2 = $("#secret_key_2");
 
         this.model = new AccountModel($client_id.val(), $api_key_1.val(), $api_key_2.val(), $secret_key_1.text(), $secret_key_2.text());
         this.view = new AccountsView( this.model );
