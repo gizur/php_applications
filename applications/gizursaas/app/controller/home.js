923a046738ef5beac47dfdce2fdd12cabf4f405c
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-01-25 15:15:53 +0530

    Secret key generation code added.

--- applications/gizursaas/app/controller/home.js
+++ applications/gizursaas/app/controller/home.js
@@ -1,5 +1,5 @@
+var account_controller = new AccountsController();
 $(function() {
-    var account_controller = new AccountsController();
     $('#generateNewAPIAndSecretKey1Button').click(function(){
         account_controller.model.emit('generateAPIKeyAndSecret1');
     });
