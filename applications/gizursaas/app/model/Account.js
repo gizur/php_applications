39caad6967c80788456f26410b4d42a750d386d2
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-01-25 15:17:05 +0530

    Secret key generation code added.

--- applications/gizursaas/app/model/Account.js
+++ applications/gizursaas/app/model/Account.js
@@ -26,10 +26,15 @@ var AccountModel = Stapes.subclass({
     'mapValues' : function(){
         //$('#email').val(_data.result.id);
         $('#api_key_1').val(this.api_key_1);
+        console.log(this.api_key_1);
         $('#api_key_2').val(this.api_key_2);
+        console.log(this.api_key_2);
         $('#secret_key_1').text(this.secret_key_1);
+        console.log(this.secret_key_1);
         $('#secret_key_2').text(this.secret_key_2);
+        console.log(this.secret_key_2);
         //$('#old_email').val(_data.result.id);
         $('#client_id').val(this.client_id);
+        console.log(this.client_id);
     }
 });
