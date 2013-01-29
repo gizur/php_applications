6e4b82065901b8f0c411fbd2df2cd862840ebdd3
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-01-29 17:22:31 +0530

    Bikeportal html structure modified.

--- applications/cikab/bikeportal/CikabTroubleTicket/TicketcikabDetail.php
+++ applications/cikab/bikeportal/CikabTroubleTicket/TicketcikabDetail.php
@@ -14,6 +14,7 @@ $helpdeskrmfieldskey = array('1', '2', '3', '4', '6', '8', '10');
 ?>
 <input align="left" class="crmbutton small cancel"type="button" value="<?PHP echo getTranslatedString('LBL_BACK_BUTTON'); ?>" onclick="window.history.back();"/>	
 </td>
+</form>
 
 <?PHP
 global $result;
@@ -111,23 +112,24 @@ if ($ticketid != '') {
         $list .= '
 		   <tr><td colspan="4" class="detailedViewHeader"><b>' . 
             getTranslatedString('LBL_ADD_COMMENT') . '</b></td></tr>
-		   
+		   <form name="comments" action="index.php" method="post">
+		   <input type="hidden" name="module">
+		   <input type="hidden" name="action">
+		   <input type="hidden" name="fun">
+		   <input type=hidden name=ticketid value=' . $ticketid . '>
 		   <tr>
-			<td colspan="4" class="dvtCellInfo"><form name="comments" action="index.php" method="post">
-		   <input type="hidden" name="module"/>
-		   <input type="hidden" name="action"/>
-		   <input type="hidden" name="fun"/>
-		   <input type=hidden name=ticketid value=' . $ticketid . '/>
-           <textarea name="comments" 
+			<td colspan="4" class="dvtCellInfo"><textarea name="comments" 
             cols="55" rows="5" class="detailedViewTextBox"  
             onFocus="this.className=\'detailedViewTextBoxOn\'" 
             onBlur="this.className=\'detailedViewTextBox\'"></textarea></td>
-		   <input class="crmbutton small cancel" title="' . 
+		   </tr>
+		   <tr>
+			    <td><input class="crmbutton small cancel" title="' . 
             getTranslatedString('LBL_SUBMIT') . '" accesskey="S" class="small"  
                 name="submit" value="' . getTranslatedString('LBL_SUBMIT') . 
             '" style="width: 70px;" type="submit" 
-                onclick="this.form.module.value=\'HelpDesk\';this.form.action.value=\'index\';this.form.fun.value=\'updatecomment\'; if(trim(this.form.comments.value) != \'\')	return true; else return false;"/></form></td>
-		   
+                onclick="this.form.module.value=\'HelpDesk\';this.form.action.value=\'index\';this.form.fun.value=\'updatecomment\'; if(trim(this.form.comments.value) != \'\')	return true; else return false;"/></td>
+		   </form>
 			    <td colspan="2">&nbsp;</td>
 			    <td>&nbsp;</td>
 		   </tr>';
@@ -203,11 +205,16 @@ if ($ticketid != '') {
     $list .= '
 		</table>
 	 </td>
-   </tr>';
+   </tr>
+</table>
+</td></tr>
+
+';
 
 
     echo $list;
-    echo '</table></td></tr>'; //</td></tr></table>
+    echo '</table></td></tr>';
+    echo '</table></td></tr></table></td></tr></table>';
     echo '<!-- --End--  -->';
 }
 else
