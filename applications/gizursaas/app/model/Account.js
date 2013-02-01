f9a7daa4a0af735beb68d1cbbde268caae9ed8cc
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-02-01 14:00:00 +0530

    Accounts model configured in Gizur SaaS.

--- applications/gizursaas/app/model/Account.js
+++ applications/gizursaas/app/model/Account.js
@@ -14,15 +14,15 @@ var AccountModel = Stapes.subclass({
         return true;
     },
     'map_values' : function(){
-        $('#first_name').val(this.first_name);
-        $('#last_name').val(this.last_name);
-        $('#email').val(this.email);
-        $('#old_email').val(this.email);
+        $('#first_name').val(this.get('first_name'));
+        $('#last_name').val(this.get('last_name'));
+        $('#email').val(this.get('email'));
+        $('#old_email').val(this.get('email'));
         
-        $('#api_key_1').val(this.api_key_1);
-        $('#api_key_2').val(this.api_key_2);
-        $('#secret_key_1').text(this.secret_key_1);
-        $('#secret_key_2').text(this.secret_key_2);
-        $('#client_id').val(this.client_id);
+        $('#api_key_1').val(this.get('api_key_1'));
+        $('#api_key_2').val(this.get('api_key_2'));
+        $('#secret_key_1').text(this.get('secret_key_1'));
+        $('#secret_key_2').text(this.get('secret_key_2'));
+        $('#client_id').val(this.get('client_id'));
     }
 });
