9ec02f5896e3ba34377b98a7422e72ce572273b1
Anshuk <gizur-ess-anshuk@gizur.com>
2013-02-07 16:42:47 +0530

    adding auto recalculate via webservice

--- lib/vtiger-5.4.0/webservice.php
+++ lib/vtiger-5.4.0/webservice.php
@@ -30,7 +30,7 @@
     if (isset($_GET['clientid']))
     if (!file_exists('user_privileges/user_privileges_' . $_GET['clientid'] . 'php')){
         RecalculateSharingRules();
-        $ourFileHandle = fopen('user_privileges/user_privileges_' . $_GET['clientid'] . 'php', 'w');
+        $ourFileHandle = fopen('user_privileges/user_privileges_' . $_GET['clientid'] . '.php', 'w');
         fclose($ourFileHandle);        
     }
 
