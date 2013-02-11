935d963ab29db5c06cb833d4a045ab91d6700de1
Anshuk <gizur-ess-anshuk@gizur.com>
2013-02-11 14:36:06 +0530

    adding mysql backup cron

--- api/protected/controllers/ApiController.php
+++ api/protected/controllers/ApiController.php
@@ -351,7 +351,7 @@ class ApiController extends Controller
             );
             
             //First we validate the model
-            if (isset($_GET['model']))
+            if (!isset($_GET['model']))
                 throw new Exception('Model not present');
             
             if (in_array($_GET['model'], $this->_valid_models)===false)
