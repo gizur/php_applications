12bc692301925ee44974fa6413b7cdf7fa347471
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-02-14 11:54:01 +0530

    Event binded in Gizur SaaS

--- applications/gizursaas/app/view/AccountsView.js
+++ applications/gizursaas/app/view/AccountsView.js
@@ -5,7 +5,7 @@ var AccountsView = Stapes.subclass({
         var self = this;
         $.get('templates/home.tmp.html?_=' + Math.random(),{},function(html){
             $('#container').empty().html(html);
-            this.bindEventHandlers();
+            self.bindEventHandlers();
         });
     },
     'success' : function(msg){
