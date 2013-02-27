0002118e7370fae9cbbc4d0b53b56c2f228238fb
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-02-27 17:17:30 +0530

    phpcronjob2 completed

--- applications/cikab/php_batches/php-interfaces/sales_orders/phpcronjob2.sh
+++ applications/cikab/php_batches/php-interfaces/sales_orders/phpcronjob2.sh
@@ -5,29 +5,35 @@ include '../config.database.php';
 
 openlog("phpcronjob1", LOG_PID | LOG_PERROR, LOG_LOCAL0);
 
-$integrationConnect = new Connect(
-        $dbconfig_integration['db_server'],
-        $dbconfig_integration['db_username'],
-        $dbconfig_integration['db_password'],
-        $dbconfig_integration['db_name']);
+try {
+    $integrationConnect = new Connect(
+            $dbconfig_integration['db_server'],
+            $dbconfig_integration['db_username'],
+            $dbconfig_integration['db_password'],
+            $dbconfig_integration['db_name']);
 
-$_messages = array();
+    $_messages = array();
 
-$salesOrders = $integrationConnect->query("SELECT salesorder_no, accountname 
+    $salesOrders = $integrationConnect->query("SELECT salesorder_no, accountname 
            FROM salesorder_interface
            WHERE sostatus IN ('created', 'approved') 
            GROUP BY salesorder_no, accountname LIMIT 0, " . $dbconfig_batchvaliable['batch_valiable']);
 
-if ($salesOrders) {
-    if ($salesOrders->num_rows > 0) {
+    if (!$salesOrders)
+        throw new Exception("Error executing sales order query : ($integrationConnect->errno) - $integrationConnect->error");
 
-        $_messages['salesOrders'] = $salesOrders->num_rows;
-        // Cycle through results
-        while ($salesOrder = $salesOrders->fetch_object()) {
+    if ($salesOrders->num_rows == 0)
+        throw new Exception("No SalesOrder Found!");
+
+    $_messages['no_sales_order'] = $salesOrders->num_rows;
+    // Cycle through results
+    while ($salesOrder = $salesOrders->fetch_object()) {
+
+        try {
 
             $integrationConnect->autocommit(FALSE);
             $flag = true;
-            $_messages[$salesOrder->salesorder_no] = array();
+            $_messages['sales_orders'][$salesOrder->salesorder_no] = array();
 
             $createdDate = date("YmdHi");
 
@@ -50,108 +56,126 @@ if ($salesOrders) {
             $productquantitylength = "";
             $leadzeroproductquantity = "";
 
-            if ($salesOrderWithProducts) {
-                while ($salesOrderWithProduct = $salesOrderWithProducts->fetch_object()) {
-                    
-                    /**
-                     * for check duplicate product and write productname in set file with+
-                     */
-                    if (!in_array($salesOrderWithProduct->productname, $productnamearray)) {
-                        $productlength = strlen($salesOrderWithProduct->productname);
-                        $productquantitylength = strlen($salesOrderWithProduct->productquantity);
+            $_messages['sales_orders'][$salesOrder->salesorder_no]['no_products'] = $salesOrderWithProducts->num_rows;
 
-                        /**
-                         * for product name length count if product name length less the 6 then auto add zero berfor the product name
-                         */
-                        if ($productlength < 6) {
-                            $leadzeroproduct = leadingzero($productlength);
-                        }
+            if (!$salesOrderWithProducts)
+                throw new Exception("Problem in fetching products.");
 
-                        /**
-                         * for product quantity length count if product quantity length less the 3 then auto add zero berfor the product quantity
-                         */
-                        if ($productquantitylength < 3) {
-                            $leadzeroproductquantity = leadingzero(3, $productquantitylength);
-                        }
+            while ($salesOrderWithProduct = $salesOrderWithProducts->fetch_object()) {
 
-                        $multiproduct[] = "189" . $leadzeroproduct . $salesOrderWithProduct->productname . $leadzeroproductquantity . $salesOrderWithProduct->productquantity;
-                        $productnamearray[] = $salesOrderWithProduct->productname;
+                /**
+                 * for check duplicate product and write productname in set file with+
+                 */
+                if (!in_array($salesOrderWithProduct->productname, $productnamearray)) {
+                    $productlength = strlen($salesOrderWithProduct->productname);
+                    $productquantitylength = strlen($salesOrderWithProduct->productquantity);
+
+                    if ($productlength < 6) {
+                        $leadzeroproduct = leadingzero($productlength);
                     }
 
-                    /**
-                     * for check duplicate account name and write account name in set file
-                     */
-                    if (!in_array($salesOrderWithProduct->accountname, $productaccountarray)) {
-                        /**
-                         * count account name length if length less then 6 then leading zero in account name.
-                         */
-                        $accountlenth = strlen($salesOrderWithProduct->accountname);
-                        if ($accountlenth < 6) {
-                            $leadzero = leadingzero(6, $accountlenth);
-                        }
-                        /**
-                         * final account name;
-                         * $accountname
-                         */
-                        $finalformataccountname = $leadzero . $salesOrderWithProduct->accountname;
+                    if ($productquantitylength < 3) {
+                        $leadzeroproductquantity = leadingzero(3, $productquantitylength);
                     }
 
-                    $finalformatproductname = implode("+", $multiproduct);
-                    $currentdate = date("Ymd");
-                    $originalordernomber = "7777" . $salesOrderWithProduct->salesorder_no;
-
-                    /**
-                     * for find the order no. total length if length will be greater then 6 then auto remove from the starting
-                     */
-                    $orderlength = strlen($originalordernomber);
-
-                    if ($orderlength > 6) {
-                        $accessorderlength = $orderlength - 6;
-                        /**
-                         *  auto remove order no.  
-                         */
-                        $ordernumber = substr($originalordernomber, $accessorderlength);
-                    } else {
-                        $ordernumber = $originalordernomber;
-                    }
-                    
-                    $deliveryday = date("ymd", strtotime($salesOrderWithProduct->duedate));
-                    $futuredeliverydate1 = strtotime(date("Y-m-d", strtotime($salesOrderWithProduct->duedate)) . " +1 day");
-                    $futuredeliverydate = date('ymd', $futuredeliverydate1);
-
-                    $orderrefferenceno = "000" . $salesOrderWithProduct->salesorder_no;
-                    
-                    $flag = $flag && true;
-                    
-                    unset($multiproduct);
-                    unset($productnamearray);
+                    $multiproduct[] = "189" . $leadzeroproduct . $salesOrderWithProduct->productname . $leadzeroproductquantity . $salesOrderWithProduct->productquantity;
+                    $productnamearray[] = $salesOrderWithProduct->productname;
                 }
-                
-                if($flag){
-                    echo $string = "HEADERGIZUR           " . $currentdate . 
-                        "18022800M256      RUTIN   .130KF27777100   " . 
-                        "mottagning initierad                               " . 
-                        "                                          001" . 
-                        $finalformataccountname . "1+03751+038" . $ordernumber . 
-                        "+226" . $futuredeliverydate . "+039" . 
-                        $deliveryday . "+040" . $ordernumber . "+" . 
-                        $finalformatproductname . "+C         RUTIN   " . 
-                        ".130KF51125185   Mottagning avslutad    " . 
-                        "BYTES/BLOCKS/RETRIES=1084 /5    /0";
+
+                /**
+                 * for check duplicate account name and write account name in set file
+                 */
+                if (!in_array($salesOrderWithProduct->accountname, $productaccountarray)) {
+
+                    $accountlenth = strlen($salesOrderWithProduct->accountname);
+                    if ($accountlenth < 6) {
+                        $leadzero = leadingzero(6, $accountlenth);
+                    }
+                    $finalformataccountname = $leadzero . $salesOrderWithProduct->accountname;
                 }
-            } else {
-                $flag = $flag && false;
+
+                $finalformatproductname = implode("+", $multiproduct);
+                $currentdate = date("Ymd");
+                $originalordernomber = "7777" . $salesOrderWithProduct->salesorder_no;
+
+                /**
+                 * for find the order no. total length if length 
+                 * will be greater then 6 then auto remove from the starting
+                 */
+                $orderlength = strlen($originalordernomber);
+
+                if ($orderlength > 6) {
+                    $accessorderlength = $orderlength - 6;
+
+                    $ordernumber = substr($originalordernomber, $accessorderlength);
+                } else
+                    $ordernumber = $originalordernomber;
+
+                $deliveryday = date("ymd", strtotime($salesOrderWithProduct->duedate));
+                $futuredeliverydate1 = strtotime(date("Y-m-d", strtotime($salesOrderWithProduct->duedate)) . " +1 day");
+                $futuredeliverydate = date('ymd', $futuredeliverydate1);
+
+                unset($multiproduct);
+                unset($productnamearray);
             }
+
+            $salesOrderWithProducts->close();
+
+            if (!$flag)
+                throw new Exception("Problem generating file content.");
+
+            $_content = "HEADERGIZUR           " . $currentdate .
+                "18022800M256      RUTIN   .130KF27777100   " .
+                "mottagning initierad                               " .
+                "                                          001" .
+                $finalformataccountname . "1+03751+038" . $ordernumber .
+                "+226" . $futuredeliverydate . "+039" .
+                $deliveryday . "+040" . $ordernumber . "+" .
+                $finalformatproductname . "+C         RUTIN   " .
+                ".130KF51125185   Mottagning avslutad    " .
+                "BYTES/BLOCKS/RETRIES=1084 /5    /0";
+
+            $_messageQ = array();
+
+            // Send File name & content to SQS 
+            $_messageQ['file'] = $fileName;
+            $_messageQ['content'] = $_content;
+
+            $_response = $sqs->send_message($amazonqueue_config['_url'], json_encode($_messageQ));
+
+            if ($_response->status !== 200)
+                throw new Exception("Error in sending file to messageQ.");
+
+            $updateStatus = $integrationConnect->query("UPDATE salesorder_interface
+                            SET sostatus = 'Delivered' where salesorder_no = '$salesOrder->salesorder_no'");
+
+            if (!$updateStatus)
+                throw new Exception("Error updating status to delivered.");
+
+            updateMessage(&$_messages, $salesOrder->salesorder_no, true, $fileName, "Successfully sent to messageQ.");
+            $integrationConnect->commit();
+        } catch (Exception $e) {
+            $integrationConnect->rollback();
+            updateLogMessage(&$_messages, $salesOrder->salesorder_no, false, $fileName, $e->getMessage());
         }
-    } else {
-        $_messages['message'] = "No SalesOrder Found!";
     }
-} else {
-    $_messages['message'] = "Error executing sales order query : ($integrationConnect->errno) - $integrationConnect->error";
+
+    $salesOrders->close();
+} catch (Exception $e) {
+    $_messages['message'] = $e->getMessage();
 }
+
+$integrationConnect->close();
 ?>
 
 <?php
+
+function updateLogMessage($m, $so, $status, $filename, $msg)
+{
+    $m['sales_orders'][$so]['status'] = $status;
+    $m['sales_orders'][$so]['file'] = $filename;
+    $m['sales_orders'][$so]['message'] = $msg;
+}
 
 /**
  * auto adding zero befor number  
@@ -165,29 +189,4 @@ function leadingzero($limitnumber = 6, $number)
     }
     return $leadzero;
 }
-
-/**
- *   Create function For Insert salesorder no into the msg que table when successfully created file and que in messagemq server.
- */
-function InsertRecordToMsg($db, $accountname, $Setfile, $connf)
-{
-
-    $Insertsalesordertable = "INSERT INTO `$db`.`saleorder_msg_que` 
-        SET accountname= '$accountname', 
-            ftpfilename= '$Setfile'  ON DUPLICATE KEY UPDATE accountname = '$accountname'";
-    $exequery = @mysql_query($Insertsalesordertable, $connf);
-    if ($exequery) {
-        $updateinterfasesatus = "UPDATE `$db`.`salesorder_interface` 
-            SET sostatus = 'Delivered' where accountname = '$accountname'";
-        $exequery = mysql_query($updateinterfasesatus, $connf);
-        if ($exequery) {
-            return true;
-        } else {
-            return false;
-        }
-    } else {
-        return false;
-    }
-}
 ?>
-
