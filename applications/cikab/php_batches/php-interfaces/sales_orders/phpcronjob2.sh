ae994d4e968f1f7f9036d944ee912daf1d180b8e
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-03-02 15:45:00 +0530

    Some comments added in cron job 3

--- applications/cikab/php_batches/php-interfaces/sales_orders/phpcronjob2.sh
+++ applications/cikab/php_batches/php-interfaces/sales_orders/phpcronjob2.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/php
 <?php
+/*
+ * Load the required files.
+ */
 require_once __DIR__ . '/../config.inc.php';
 require_once __DIR__ . '/../config.database.php';
 require_once __DIR__ . '/../config.sqs.inc.php';
