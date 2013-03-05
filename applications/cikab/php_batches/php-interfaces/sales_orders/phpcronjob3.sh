4780f600eca1299ac66508513d8f36184c54a4f8
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-03-05 11:25:51 +0530

    Linting done!

--- applications/cikab/php_batches/php-interfaces/sales_orders/phpcronjob3.sh
+++ applications/cikab/php_batches/php-interfaces/sales_orders/phpcronjob3.sh
@@ -9,11 +9,13 @@ require_once __DIR__ . '/../config.sqs.inc.php';
 /*
  * Open connection to the system logger
  */
-openlog("phpcronjob3", LOG_PID | LOG_PERROR, LOG_LOCAL0);
+openlog(
+    "phpcronjob3", LOG_PID | LOG_PERROR, LOG_LOCAL0
+);
 /*
  * Message array to store logs
  */
-$_messages = array();
+$messages = array();
 
 /*
  * Try block to catch the exceptions
@@ -22,7 +24,8 @@ try {
     /*
      * Get number of files stored in message queue.
      */
-    $messageCount = $no_of_files = $sqs->get_queue_size($amazonqueueConfig['_url']);
+    $messageCount = $sqs->get_queue_size($amazonqueueConfig['_url']);
+    $noOfFiles = $messageCount;
 
     /*
      * If number of files are 0, throw the exception
@@ -42,78 +45,93 @@ try {
             /*
              * Get the single message from the messageQ.
              */
-            $_response = $sqs->receive_message($amazonqueueConfig['_url']);
+            $responseQ = $sqs->receive_message($amazonqueueConfig['_url']);
 
             /*
              * If response is 200, Throw exception.
              */
-            if (!$_response->status == 200)
-                throw new Exception("Message not recieved from the messageQ server.");
+            if (!$responseQ->status == 200)
+                throw new Exception(
+                    "Message not recieved from the messageQ server."
+                );
 
             /*
              * Get the message body.
              */
-            $msgObj = $_response->body->ReceiveMessageResult->Message;
+            $msgObj = $responseQ->body->ReceiveMessageResult->Message;
 
             /*
              * If message body is empty, raise the exception.
              */
             if (empty($msgObj))
-                throw new Exception("Received an empty message from messageQ.");
+                throw new Exception(
+                    "Received an empty message from messageQ."
+                );
 
             /*
              * File name and content were json encoded so decode it.
              */
-            $_fileJson = json_decode($msgObj->Body);
+            $fileJson = json_decode($msgObj->Body);
             /*
-             * Get the message receipt, It will be used in deleting the message
+             * Get the message receipt, It will be 
+             * used in deleting the message
              * from messageQ.
              */
-            $_receipt = $msgObj->ReceiptHandle;
+            $receiptQ = $msgObj->ReceiptHandle;
 
             /*
              * If file content are empty raise the exception.
              */
-            if(empty($_fileJson->content))
-                throw new Exception("$_fileJson->file content is empty in messageQ.");
+            if(empty($fileJson->content))
+                throw new Exception(
+                    "$fileJson->file content is empty in messageQ."
+                );
             
-            $ServerFilePath = $dbconfigFtp['serverpath'];
-            $ftp_path = $ServerFilePath . $_fileJson->file;
+            $serverFilePath = $dbconfigFtp['serverpath'];
+            $ftpPath = $serverFilePath . $fileJson->file;
             
             /*
              * If file exists at FTP, raise the Exception.
              */
-            if (file_exists($ftp_path))
-                throw new Exception("$_fileJson->file file already exists at FTP server.");
+            if (file_exists($ftpPath))
+                throw new Exception(
+                    "$fileJson->file file already exists at FTP server."
+                );
             
             /*
              * Prepare file in temp dir locally.
              */
             $fp = fopen('php://temp', 'r+');
-            fwrite($fp, $_fileJson->content);
+            fwrite($fp, $fileJson->content);
             rewind($fp);
             /*
              * Upload file to FTP.
              */
-            $uploaded = ftp_fput($ftpConnId, $ftp_path, $fp, FTP_ASCII);
+            $uploaded = ftp_fput(
+                $ftpConnId, $ftpPath, $fp, FTP_ASCII
+            );
             fclose($fp);
             /*
              * If file upload process fails, throw the exception.
              */
             if(!$uploaded)
-                throw new Exception("Error copying file $_fileJson->file on FTP server.");
+                throw new Exception(
+                    "Error copying file $fileJson->file on FTP server."
+                );
 
             /*
              * If file processed, delete message from messageQ.
              */
-            $sqs->delete_message($amazonqueueConfig['_url'], $_receipt);
+            $sqs->delete_message(
+                $amazonqueueConfig['_url'], $receiptQ
+            );
             
-            $_messages['files'][$_fileJson->file] = true;
+            $messages['files'][$fileJson->file] = true;
             /*
              * Catch the exceptions
              */
         } catch (Exception $e) {
-            $_messages[] = $e->getMessage();
+            $messages[] = $e->getMessage();
         }
         /*
          * Decrease $messageCount by 1
@@ -123,13 +141,12 @@ try {
     /*
      * Update the success message
      */
-    $_messages['message'] = "Total $no_of_files files copied.";
+    $messages['message'] = "Total $noOfFiles files copied.";
 } catch (Exception $e) {
-    $_messages['message'] = $e->getMessage();
+    $messages['message'] = $e->getMessage();
 }
 /*
  * Update system logs and print log messages.
  */
-syslog(LOG_WARNING, json_encode($_messages));
-echo json_encode($_messages);
-?>
+syslog(LOG_WARNING, json_encode($messages));
+echo json_encode($messages);
