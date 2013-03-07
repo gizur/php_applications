983132792d3c34d86e250007b10e1b1889604873
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-03-07 13:51:53 +0530

    Cron Job 1 test added

---
+++ applications/cikab/tests/ftp.js
@@ -0,0 +1,5 @@
+var ftpd = require('ftp-server');
+// Path to your FTP root
+ftpd.fsOptions.root = './files';
+// Start listening on port 21 (you need to be root for ports < 1024)
+ftpd.listen(21);
--- applications/cikab/tests/test_cronjob1.js
+++ applications/cikab/tests/test_cronjob1.js
@@ -3,6 +3,7 @@ AWS.config.loadFromPath('./_secure/credentials.json');
 var mysql = require("mysql");
 
 var http    = require('http');
+var fs = require('fs');
 
 var config  = require('./_secure/config.js').Config;
 
@@ -14,6 +15,13 @@ var connection = mysql.createConnection({
     user: config.DB_USER,
     password: config.DB_PASSWORD,
     database: config.DB_NAME
+});
+
+var int_connection = mysql.createConnection({
+    host: config.DB_I_HOST,
+    user: config.DB_I_USER,
+    password: config.DB_I_PASSWORD,
+    database: config.DB_I_NAME
 });
 
 // Accept one parameter
@@ -55,16 +63,15 @@ function testPhpBatch(test, batch){
     req.end();
 }
 
+connection.connect();
+int_connection.connect();
+
 exports.group = {
     "Testing Sales Order Interface Cron Job 1" : function(test){
         test.ok(true, "This test will always pass.");
         test.done();
     },
-    "Run Cron Job 1" : function(test){
-        testPhpBatch(test, config.PHP_BATCHES_1);
-    },
-    "Checking Sales Orders in vTiger ('Created','Approved')" : function(test){
-        connection.connect();
+    "Checking Sales Orders in vTiger ('Created','Approved') before hitting cron job 1" : function(test){
         connection.query("SELECT SO.salesorderid, SO.salesorder_no FROM " +
             "vtiger_salesorder SO " + 
             "WHERE SO.sostatus IN ('Created','Approved') " +
@@ -72,6 +79,32 @@ exports.group = {
                 if (err) throw err;
                 
                 test.equal(rows.length, 1, rows.length + " sales orders found in vTiger.");
+                test.done();
+            });
+    },
+    "Hitting Cron Job 1" : function(test){
+        testPhpBatch(test, config.PHP_BATCHES_1);
+    },
+    "Checking Sales Orders in vTiger ('Created','Approved') after hitting cron job 1" : function(test){
+        connection.query("SELECT SO.salesorderid, SO.salesorder_no FROM " +
+            "vtiger_salesorder SO " + 
+            "WHERE SO.sostatus IN ('Created','Approved') " +
+            "LIMIT 0, 10", function(err, rows, fields) {
+                if (err) throw err;
+                
+                test.equal(rows.length, 0, rows.length + " sales orders found in vTiger.");
+                test.done();
+            });
+    },
+    "Checking Sales Order In Integration Database" : function(test){
+        int_connection.query("SELECT salesorder_no, " +
+         "accountname " +
+         "FROM salesorder_interface " +
+         "WHERE sostatus IN ('created', 'approved') " +
+         "GROUP BY salesorder_no, accountname", function(err, rows, fields) {
+                if (err) throw err;
+                
+                test.equal(rows.length, 1, rows.length + " sales orders found in integration db.");
                 test.done();
             });
     }
