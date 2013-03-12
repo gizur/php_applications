3c69dca2fdc3056335c5a50c37d94a45ca3a5976
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-03-12 12:56:05 +0530

    testbatches modified

--- applications/cikab/php_batches/php-interfaces/sales_orders/testbatches.php
+++ applications/cikab/php_batches/php-interfaces/sales_orders/testbatches.php
@@ -5,24 +5,24 @@ error_reporting(E_ALL);
 $output = null;
 
 if (isset($_GET['action'])) {
-    @shell_exec('sudo chmod +x ' . $_GET['action'] . '.sh');
+    @shell_exec('sudo chmod +x ' . __DIR__ . $_GET['action'] . '.sh');
     switch ($_GET['action']) {
         case 'setup-tables':            
-            $output = shell_exec('./setup-tables.sh');
+            $output = shell_exec(__DIR__ . '/setup-tables.sh');
             break;
         case 'phpcronjob1':
-            $output = shell_exec('./phpcronjob1.sh');
+            $output = shell_exec(__DIR__ . '/phpcronjob1.sh');
             break;
         case 'phpcronjob2':
-            $output = shell_exec('./phpcronjob2.sh');
+            $output = shell_exec(__DIR__ . '/phpcronjob2.sh');
             break;
         case 'phpcronjob3':
-            $output = shell_exec('./phpcronjob3.sh');
+            $output = shell_exec(__DIR__ . '/phpcronjob3.sh');
             break;
         case 'phpinfo':
             phpinfo();
             break;
     }
-    @shell_exec('sudo chmod -x ' . $_GET['action'] . '.sh');
+    @shell_exec('sudo chmod -x ' . __DIR__ . $_GET['action'] . '.sh');
     echo $output;
 }
