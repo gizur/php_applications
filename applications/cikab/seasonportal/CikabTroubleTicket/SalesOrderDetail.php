3718ceff04cb785a34943e902a968c66b7eb468b
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-03-25 14:59:02 +0530

    Back button added in season portal

--- applications/cikab/seasonportal/CikabTroubleTicket/SalesOrderDetail.php
+++ applications/cikab/seasonportal/CikabTroubleTicket/SalesOrderDetail.php
@@ -1,4 +1,5 @@
 <?php
+echo '<table><tr><td><input class="crmbutton small cancel" type="button" value="'.getTranslatedString('LBL_BACK_BUTTON').'" onclick="window.history.back();"/></td></tr></table>';
 if (isset($_REQUEST['salesorder_no']) && !empty($_REQUEST['salesorder_no']))
     echo str_replace('${number}', $_REQUEST['salesorder_no'], getTranslatedString('LBL_SUCCESS_IN_SAVING_SALESORDER'));
 else
