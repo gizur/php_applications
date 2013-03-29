f487fe403abac2cd784c8ec68b40d312fac3d347
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-03-29 11:56:43 +0530

    Forgot password added ion gizur saas

--- applications/gizursaas/config/config.js
+++ applications/gizursaas/config/config.js
@@ -21,6 +21,6 @@ var __rest_server_url = 'http://localhost/php_applications/api/';
 var __messages = {
     "CLIENT_ID_INVALID" : 'Client ID is not available.',
     "EMAIL_INVALID" : 'Email is already registred.',
-    "ERROR" : 'An error occured while creating your account, Please contact administrator.',
+    "ERROR" : 'An error occured, Please contact administrator.',
     "WRONG_CREDENTIALS" : 'Credentials are in valid.'
 };
