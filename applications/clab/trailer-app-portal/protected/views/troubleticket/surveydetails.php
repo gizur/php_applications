73c96e6dba890558b79879eca515c7fea9d12bbb
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-05-27 16:30:13 +0530

    Fields updated in trailer portal

--- applications/clab/trailer-app-portal/protected/views/troubleticket/surveydetails.php
+++ applications/clab/trailer-app-portal/protected/views/troubleticket/surveydetails.php
@@ -1,147 +1,145 @@
 <!-- 
 /**
-	 * 
-	 * 
-	 * Created date : 04/07/2012
-	 * Created By : Anil Singh
-	 * @author Anil Singh <anil-singh@essindia.co.in>
-	 * Flow : The basic flow of this page is Details of Trouble tickets.
-	 * Modify date : 13/08/2012
-	*/
+     * 
+     * 
+     * Created date : 04/07/2012
+     * Created By : Anil Singh
+     * @author Anil Singh <anil-singh@essindia.co.in>
+     * Flow : The basic flow of this page is Details of Trouble tickets.
+     * Modify date : 13/08/2012
+    */
 
 -->
-<?php 
-include_once 'protected/extensions/language/'.Yii::app()->session['Lang'].'.php';
-?>
-<?php  
-  $baseUrl = Yii::app()->baseUrl; 
-  $cs = Yii::app()->getClientScript();
-  $cs->registerScriptFile($baseUrl.'/assets/a719a609/jquery.js');
+<?php
+include_once 'protected/extensions/language/' . Yii::app()->session['Lang'] . '.php';
 ?>
 <?php
-$this->pageTitle=Yii::app()->name . ' - New Ticket for Survey ';
+$baseUrl = Yii::app()->baseUrl;
+$cs = Yii::app()->getClientScript();
+$cs->registerScriptFile($baseUrl . '/assets/a719a609/jquery.js');
+?>
+<?php
+$this->pageTitle = Yii::app()->name . ' - New Ticket for Survey ';
 
-echo CHtml::metaTag($content='My page description', $name='decription');
+echo CHtml::metaTag($content = 'My page description', $name = 'decription');
 
-$this->breadcrumbs=array(
-        getTranslatedString('Trouble Ticket').'/'.getTranslatedString('Survey Details'),
+$this->breadcrumbs = array(
+    getTranslatedString('Trouble Ticket') . '/' . getTranslatedString('Survey Details'),
 );
 ?>
 <div style="float:right; margin-bottom:10px" class="button">
-<a href="index.php?r=troubleticket/surveylist/"><?php echo getTranslatedString('Trouble ticket List');?></a></div>
+    <a href="index.php?r=troubleticket/surveylist/"><?php echo getTranslatedString('Trouble ticket List'); ?></a></div>
 
 <div style="background:#E5E5E5; width:550px"><strong>Ticket Information : <?php echo $result['result']['ticket_title']; ?></strong></div>	
 <div class="Survey">
 
-<h2><?php echo getTranslatedString('Survey');?></h2>
-<table width="100%" border="0" bgcolor="#589fc8" cellspacing="1" cellpadding="5">
-<tr>
-    <td width="26%" bgcolor="e3f0f7"><strong><?php echo getTranslatedString('Ticket ID');?></strong></td>
-    <td width="74%" bgcolor="e3f0f7"> <?php echo $result['result']['ticket_no']; ?></td>
-  </tr>
-   <tr>
-    <td bgcolor="e3f0f7"><strong><?php echo getTranslatedString('Trailer ID');?></strong></td>
-    <td bgcolor="e3f0f7"> <?php echo $result['result']['trailerid']; ?></td>
-  </tr>
-    <tr>
-    <td bgcolor="e3f0f7"><strong><?php echo getTranslatedString('Date');?></strong></td>
-    <td bgcolor="e3f0f7"><?php echo date('Y-m-d',strtotime($result['result']['createdtime'])); ?></td>
-  </tr>
-    <tr>
-    <td bgcolor="e3f0f7"><strong><?php echo getTranslatedString('Time');?></strong></td>
-    <td bgcolor="e3f0f7"><?php echo date('h:i',strtotime($result['result']['createdtime'])); ?></td>
-  </tr>
-  <tr>
-    <td bgcolor="e3f0f7"><strong><?php echo getTranslatedString('Place');?> </strong></td>
-    <td bgcolor="e3f0f7"><?php echo $result['result']['damagereportlocation']; ?></td>
-  </tr>
-  <tr>
-    <td bgcolor="e3f0f7"><strong><?php echo getTranslatedString('Plates');?> </strong></td>
-    <td bgcolor="e3f0f7"><?php echo $result['result']['plates']; ?></td>
-  </tr>
-  <tr>
-    <td bgcolor="e3f0f7"><strong><?php echo getTranslatedString('Straps');?> </strong></td>
-    <td bgcolor="e3f0f7"><?php echo $result['result']['straps']; ?></td>
-  </tr>  
-    <tr>
-    <td bgcolor="e3f0f7"><strong><?php echo getTranslatedString('Account');?></strong></td>
-    <td bgcolor="e3f0f7"><?php echo $result['result']['accountname']; ?></td>
-  </tr>
-    <tr>
-    <td bgcolor="e3f0f7"><strong><?php echo getTranslatedString('Contact');?></strong></td>
-    <td bgcolor="e3f0f7"><?php echo $result['result']['contactname']['firstname'] . "&nbsp;".$result['result']['contactname']['lastname']; ?></td>
-  </tr>
- 
-  </tr>
-</table>
+    <h2><?php echo getTranslatedString('Survey'); ?></h2>
+    <table width="100%" border="0" bgcolor="#589fc8" cellspacing="1" cellpadding="5">
+        <tr>
+            <td width="26%" bgcolor="e3f0f7"><strong><?php echo getTranslatedString('Ticket ID'); ?></strong></td>
+            <td width="74%" bgcolor="e3f0f7"> <?php echo $result['result']['ticket_no']; ?></td>
+        </tr>
+        <tr>
+            <td bgcolor="e3f0f7"><strong><?php echo getTranslatedString('Trailer ID'); ?></strong></td>
+            <td bgcolor="e3f0f7"> <?php echo $result['result']['trailerid']; ?></td>
+        </tr>
+        <tr>
+            <td bgcolor="e3f0f7"><strong><?php echo getTranslatedString('Date'); ?></strong></td>
+            <td bgcolor="e3f0f7"><?php echo date('Y-m-d', strtotime($result['result']['createdtime'])); ?></td>
+        </tr>
+        <tr>
+            <td bgcolor="e3f0f7"><strong><?php echo getTranslatedString('Time'); ?></strong></td>
+            <td bgcolor="e3f0f7"><?php echo date('h:i', strtotime($result['result']['createdtime'])); ?></td>
+        </tr>
+        <tr>
+            <td bgcolor="e3f0f7"><strong><?php echo getTranslatedString('Place'); ?> </strong></td>
+            <td bgcolor="e3f0f7"><?php echo $result['result']['damagereportlocation']; ?></td>
+        </tr>
+        <tr>
+            <td bgcolor="e3f0f7"><strong><?php echo getTranslatedString('Damage Status'); ?> </strong></td>
+            <td bgcolor="e3f0f7"><?php echo getTranslatedString($result['result']['damagestatus']); ?></td>
+        </tr>
+        <tr>
+            <td bgcolor="e3f0f7"><strong><?php echo getTranslatedString('Anteckningar'); ?> </strong></td>
+            <td bgcolor="e3f0f7"><?php echo getTranslatedString($result['result']['notes']); ?></td>
+        </tr>  
+        <tr>
+            <td bgcolor="e3f0f7"><strong><?php echo getTranslatedString('Account'); ?></strong></td>
+            <td bgcolor="e3f0f7"><?php echo $result['result']['accountname']; ?></td>
+        </tr>
+        <tr>
+            <td bgcolor="e3f0f7"><strong><?php echo getTranslatedString('Contact'); ?></strong></td>
+            <td bgcolor="e3f0f7"><?php echo $result['result']['contactname']['firstname'] . "&nbsp;" . $result['result']['contactname']['lastname']; ?></td>
+        </tr>
+
+        </tr>
+    </table>
 </div>
 
 <div class="Damage">
 
-<h2><?php echo getTranslatedString('Damage');?></h2>
+    <h2><?php echo getTranslatedString('Damage'); ?></h2>
 
-<table width="100%" border="0" cellspacing="5" style="border:#589fc8 solid 1px; padding:5px;" cellpadding="0">
-  <tr>
-    <td width="50%" valign="top"><table width="100%" border="0" bgcolor="#589fc8" cellspacing="1" cellpadding="5">
-  <tr>
-    <td width="50%" bgcolor="7eb6d5"><?php echo getTranslatedString('Type of damage');?></td>
-    <td width="50%" bgcolor="7eb6d5">  <?php echo $result['result']['damagetype']; ?></td>
-  </tr>
-   <tr>
-    <td bgcolor="e3f0f7"><?php echo getTranslatedString('Position on trailer');?></td>
-    <td bgcolor="e3f0f7"><?php echo $result['result']['damageposition']; ?></td>
-  </tr>
-     <tr>
-    <td bgcolor="e3f0f7"><?php echo getTranslatedString('Status of damage');?></td>
-    <td bgcolor="e3f0f7" id="markdamagebutton"><?php echo $result['result']['ticketstatus']; ?></td>
-  </tr>
- 
-</table>
+    <table width="100%" border="0" cellspacing="5" style="border:#589fc8 solid 1px; padding:5px;" cellpadding="0">
+        <tr>
+            <td width="50%" valign="top"><table width="100%" border="0" bgcolor="#589fc8" cellspacing="1" cellpadding="5">
+                    <tr>
+                        <td width="50%" bgcolor="7eb6d5"><?php echo getTranslatedString('Type of damage'); ?></td>
+                        <td width="50%" bgcolor="7eb6d5">  <?php echo $result['result']['damagetype']; ?></td>
+                    </tr>
+                    <tr>
+                        <td bgcolor="e3f0f7"><?php echo getTranslatedString('Position on trailer'); ?></td>
+                        <td bgcolor="e3f0f7"><?php echo $result['result']['damageposition']; ?></td>
+                    </tr>
+                    <tr>
+                        <td bgcolor="e3f0f7"><?php echo getTranslatedString('Status of damage'); ?></td>
+                        <td bgcolor="e3f0f7" id="markdamagebutton"><?php echo $result['result']['ticketstatus']; ?></td>
+                    </tr>
 
-<br>
-<input type="button"  onclick="AjaxMarkDamage('<?php echo $result['result']['id']; ?>')" class="button" value="<?php echo getTranslatedString('Mark damage repaired');?>" />
+                </table>
+
+                <br>
+                <input type="button"  onclick="AjaxMarkDamage('<?php echo $result['result']['id']; ?>')" class="button" value="<?php echo getTranslatedString('Mark damage repaired'); ?>" />
 
 
+            </td>
+            <td width="15%" valign="top"><table width="100%" border="0" bgcolor="#589fc8" cellspacing="1" cellpadding="5">
+                    <tr>
+                        <td colspan="2" bgcolor="7eb6d5"  valign="top"><strong><?php echo getTranslatedString('Pictures'); ?></strong></td>
+                    </tr>
+                    <?php
+                    $i = 1;
+                    if (count($result['result']['documents']) > 0) {
+                        foreach ($result['result']['documents'] as $image) {
+                            echo '<td width="50%" bgcolor="e3f0f7" align="center"><img src="' . Yii::app()->request->baseUrl . '/index.php?r=troubleticket/images/' . $image['id'] . '" width="100px" height="100px"></td>';
+                            if ($i % 2 == 0) {
+                                echo "</tr><tr>";
+                            }
+                            $i++;
+                        }
+                    }
+                    ?> 
+        </tr>
+    </table>
 </td>
-    <td width="15%" valign="top"><table width="100%" border="0" bgcolor="#589fc8" cellspacing="1" cellpadding="5">
-  <tr>
-    <td colspan="2" bgcolor="7eb6d5"  valign="top"><strong><?php echo getTranslatedString('Pictures');?></strong></td>
-    </tr>
-   <?php 
-     $i=1;
-  if(count($result['result']['documents'])>0){
-   foreach($result['result']['documents'] as $image)
-   {
-       echo '<td width="50%" bgcolor="e3f0f7" align="center"><img src="'.Yii::app()->request->baseUrl.'/index.php?r=troubleticket/images/'.$image['id'].'" width="100px" height="100px"></td>';  
-         if($i%2==0)
-         {
-		  echo "</tr><tr>";
-		  } 
-		 $i++; 
-	  }
-	}  
-    ?> 
-  </tr>
-</table>
-</td>
-  </tr>
+</tr>
 </table>
 </div>
 
 <script>
 
-function AjaxMarkDamage(id)
-{
-$('#markdamagebutton').html('Please wait...');
-$("#markdamagebutton").addClass("waitprocess2");
-$.post('index.php?r=troubleticket/markdamagestatus/',{ticketid: id},
- function(data) 
-{
-$("#markdamagebutton").removeClass("waitprocess2");
-$('#markdamagebutton').html(data);
-});
-	
-}
+                    function AjaxMarkDamage(id)
+                    {
+                        $('#markdamagebutton').html('Please wait...');
+                        $("#markdamagebutton").addClass("waitprocess2");
+                        $.post('index.php?r=troubleticket/markdamagestatus/', {ticketid: id},
+                        function(data)
+                        {
+                            $("#markdamagebutton").removeClass("waitprocess2");
+                            $('#markdamagebutton').html(data);
+                        });
+
+                    }
 
 </script>
 
