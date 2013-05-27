aaea2115d910d744814ff4a6c6275b51d0b58fdb
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-05-27 11:35:08 +0530

    trailer portal configured for TEST env

--- applications/clab/trailer-app-portal/protected/views/troubleticket/survey.php
+++ applications/clab/trailer-app-portal/protected/views/troubleticket/survey.php
@@ -1,190 +1,189 @@
 <!-- 
 /**
-	 * 
-	 * 
-	 * Created date : 04/07/2012
-	 * Created By : Anil Singh
-	 * @author Anil Singh <anil-singh@essindia.co.in>
-	 * Flow : The basic flow of this page is Create of Trouble tickets (Survey).
-	 * Modify date : 05/08/2012
-	*/
+     * 
+     * 
+     * Created date : 04/07/2012
+     * Created By : Anil Singh
+     * @author Anil Singh <anil-singh@essindia.co.in>
+     * Flow : The basic flow of this page is Create of Trouble tickets (Survey).
+     * Modify date : 05/08/2012
+    */
 
 -->
-<?php 
-include_once 'protected/extensions/language/'.Yii::app()->session['Lang'].'.php';
+<?php
+include_once 'protected/extensions/language/' . Yii::app()->session['Lang'] . '.php';
 ?>
 <?php
-$this->pageTitle=Yii::app()->name . ' - New Ticket for Survey ';
+$this->pageTitle = Yii::app()->name . ' - New Ticket for Survey ';
 
-echo CHtml::metaTag($content='My page description', $name='decription');
+echo CHtml::metaTag($content = 'My page description', $name = 'decription');
 
-$this->breadcrumbs=array(
-        getTranslatedString('Trouble ticket').' /'. getTranslatedString('Create new Trouble ticket'),
+$this->breadcrumbs = array(
+    getTranslatedString('Trouble ticket') . ' /' . getTranslatedString('Create new Trouble ticket'),
 );
-
 ?>
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'troubleticketsurvey',
-	'htmlOptions' => array('enctype' => 'multipart/form-data'),
-	'enableClientValidation'=>true,
-	'clientOptions'=>array(
-	'validateOnSubmit'=>true,
-	
-	),
-)); ?>
-   <?php
-    foreach(Yii::app()->user->getFlashes() as $key => $message) {
-        echo '<div class="flash-' . $key . '">' . $message . "</div>\n";
-    }
+<?php
+$form = $this->beginWidget('CActiveForm', array(
+    'id' => 'troubleticketsurvey',
+    'htmlOptions' => array('enctype' => 'multipart/form-data'),
+    'enableClientValidation' => true,
+    'clientOptions' => array(
+        'validateOnSubmit' => true,
+    ),
+    ));
 ?>
-<div style="background:#E5E5E5"><strong><?php echo getTranslatedString('Create new Trouble ticket');?></strong></div>	
-<div align="center">
-<table style="width:100%">
-<tr>
- <td>
-   <?php echo $form->labelEx($model,getTranslatedString('Title')); ?>
-   </td><td>
-    <?php echo $form->textField($model,'Title',array('value'=> $postdata['Title'])); ?>
-    <?php echo $form->error($model,'Title'); ?>
-   
-  </td>
-
-  <td>
-  <?php echo $form->labelEx($model,getTranslatedString('Ticket Category')); ?>
-  </td><td>
-  <?php echo $form->dropDownList($model,'Category', $category); ?>
-  <?php echo $form->error($model,'Category'); ?>
-  
-  </td>
-  </tr>
-<tr>
- <td>
-
-   <?php echo $form->labelEx($model,getTranslatedString('Trailer ID')); ?>&nbsp;<span style="color:red">*</span>
-   </td><td>
-   <?php echo $form->dropDownList($model,'TrailerID', $Assets); ?>
-    <?php echo $form->error($model,'TrailerID'); ?>
-   
-  </td>
-  <td>
-  <?php echo $form->labelEx($model,getTranslatedString('Location for damage report')); ?>&nbsp;<span style="color:red">*</span>
-  </td><td>
-    <?php echo $form->textField($model,'Damagereportlocation',array('value'=> $postdata['Damagereportlocation'])); ?>
-    <?php echo $form->error($model,'Damagereportlocation'); ?>
-  
-  </td>
-  </tr>
-<tr>
-   <td>
-  <?php echo $form->labelEx($model,getTranslatedString('Sealed')); ?>
-  </td><td>
-  <?php echo $form->dropDownList($model,'Sealed',$Sealed);?>
-  </td>
-   
-    <td>
-   <?php echo $form->labelEx($model,getTranslatedString('Plates')); ?>
-  </td><td>
-   <?php echo $form->textField($model,'Plates',array('value'=> $postdata['Plates'])); ?>
-    <?php echo $form->error($model,'Plates'); ?>
-    </td> 
-   
-</tr>
-
-
-  <tr>
-<td>
-  <?php echo $form->labelEx($model,getTranslatedString('Straps')); ?>&nbsp;<span style="color:red">*</span>
-  </td><td>
-   <?php echo $form->textField($model,'Straps',array('value'=> $postdata['Straps'])); ?>
-    <?php echo $form->error($model,'Straps'); ?>
-  </td>	  
-	  
-   <td>
-  <?php echo $form->labelEx($model,getTranslatedString('Type of damage')); ?>
-  </td><td>
-    <?php echo $form->dropDownList($model,'Typeofdamage',$damagetype);?>
-      <?php echo $form->error($model,'Typeofdamage'); ?>
-  </td>
- 
-  </tr>
-  <tr>
- <td>
-  <?php echo $form->labelEx($model,getTranslatedString('Position on trailer for damage')); ?>
-  </td><td>
-     <?php echo $form->dropDownList($model,'Damageposition',$damagepos);?>
-        <?php //echo $form->hiddenField($model,'Damageposition',array('type'=>"hidden",'size'=>2,'maxlength'=>60 ,'value'=>'Höger sida (Right side)')); ?>
-    <?php echo $form->error($model,'Damageposition'); ?>
-  
-  </td>	  
-  <td>
-  <?php echo $form->labelEx($model,getTranslatedString('Driver caused damage')); ?>
-  </td><td>
-	 
-   <?php echo $form->dropDownList($model,'drivercauseddamage',$drivercauseddamageList);?>
-     </td>	
-  </tr>
-  <tr>
-
-<td>
-  <?php echo $form->labelEx($model,getTranslatedString('Damage Reported')); ?>
-  </td>
-  <td>
-     <?php echo $form->dropDownList($model,'reportdamage',$reportdamage);?>
-  </td>
-  
-  <td><?php echo $form->labelEx($model,getTranslatedString('Ticket Status')); ?></td>
-  <td><?php 
-  $ticketstatus2=array();
-  foreach($ticketstatus as $status){
-
-  if($status=='Open' || $status=='Closed')
-  {
-   $ticketstatus2[$status]=$status;
-   }
-  
-  }
-    echo $form->dropDownList($model,'ticketstatus',$ticketstatus2);?></td>
-    </tr>
-  <tr>
-<td>
-  <?php echo $form->labelEx($model,getTranslatedString('Upload Pictures')); ?>
-  </td>
- 
-  <td>
-  <?php
-    echo $form->fileField($model, 'image');
-  ?>
-  <?php echo $form->error($model,'image'); ?>
-  <span id="addmorepic"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/001_01.png" title="Add More Pictures" /></span>
-  <p id="morepic"></p> 
-  </td>
-  <td>
-  <?php echo $form->labelEx($model,getTranslatedString('Create Date')); ?>
-  </td>
-  <td>
-   <?php echo date('Y-m-d'); ?>
-   <?php echo $form->hiddenField($model,'TroubleTicketType',array('type'=>"hidden",'size'=>2,'maxlength'=>2, 'value'=>'survey')); ?>
-  </td>
-</tr>
-</table>
- <?php echo CHtml::submitButton(getTranslatedString('Submit'), array('id'=>'submit','name'=>'submit')); ?>
-<?php echo CHtml::endForm(); ?>
-<?php $this->endWidget(); ?>
-
-<script>
-var data ="<div><input type='file' name='Troubleticket[image";
-var data2 = "]' id='file";
-var data3 ="' />&nbsp;<span style='cursor:pointer'><img src='<?php echo Yii::app()->request->baseUrl; ?>/images/buttons_21.png' title='Remove Picurest'  onclick='removefiles(this)'/></span></div>";
-$("#addmorepic").click(function() {
-var counter=$('input[type=file]').length;
-$('#morepic').append(data+counter+data2+counter+data3);
-});
-
-function removefiles(obj) 
-{
-   $(obj).parents("div:first").remove();
- 
+<?php
+foreach (Yii::app()->user->getFlashes() as $key => $message) {
+    echo '<div class="flash-' . $key . '">' . $message . "</div>\n";
 }
+?>
+<div style="background:#E5E5E5"><strong><?php echo getTranslatedString('Create new Trouble ticket'); ?></strong></div>	
+<div align="center">
+    <table style="width:100%">
+        <tr>
+            <td>
+                <?php echo $form->labelEx($model, getTranslatedString('Title')); ?>
+            </td><td>
+                <?php echo $form->textField($model, 'Title', array('value' => $postdata['Title'])); ?>
+                <?php echo $form->error($model, 'Title'); ?>
 
-</script>
+            </td>
+
+            <td>
+                <?php echo $form->labelEx($model, getTranslatedString('Ticket Category')); ?>
+            </td><td>
+                <?php echo $form->dropDownList($model, 'Category', $category); ?>
+                <?php echo $form->error($model, 'Category'); ?>
+
+            </td>
+        </tr>
+        <tr>
+            <td>
+
+                <?php echo $form->labelEx($model, getTranslatedString('Trailer ID')); ?>&nbsp;<span style="color:red">*</span>
+            </td><td>
+                <?php echo $form->dropDownList($model, 'TrailerID', $Assets); ?>
+                <?php echo $form->error($model, 'TrailerID'); ?>
+
+            </td>
+            <td>
+                <?php echo $form->labelEx($model, getTranslatedString('Location for damage report')); ?>&nbsp;<span style="color:red">*</span>
+            </td><td>
+                <?php echo $form->textField($model, 'Damagereportlocation', array('value' => $postdata['Damagereportlocation'])); ?>
+                <?php echo $form->error($model, 'Damagereportlocation'); ?>
+
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <?php echo $form->labelEx($model, getTranslatedString('Sealed')); ?>
+            </td><td>
+                <?php echo $form->dropDownList($model, 'Sealed', $Sealed); ?>
+            </td>
+
+            <td>
+                <?php echo $form->labelEx($model, getTranslatedString('Plates')); ?>
+            </td><td>
+                <?php echo $form->textField($model, 'Plates', array('value' => $postdata['Plates'])); ?>
+                <?php echo $form->error($model, 'Plates'); ?>
+            </td> 
+
+        </tr>
+
+
+        <tr>
+            <td>
+                <?php echo $form->labelEx($model, getTranslatedString('Straps')); ?>&nbsp;<span style="color:red">*</span>
+            </td><td>
+                <?php echo $form->textField($model, 'Straps', array('value' => $postdata['Straps'])); ?>
+                <?php echo $form->error($model, 'Straps'); ?>
+            </td>	  
+
+            <td>
+                <?php echo $form->labelEx($model, getTranslatedString('Type of damage')); ?>
+            </td><td>
+                <?php echo $form->dropDownList($model, 'Typeofdamage', $damagetype); ?>
+                <?php echo $form->error($model, 'Typeofdamage'); ?>
+            </td>
+
+        </tr>
+        <tr>
+            <td>
+                <?php echo $form->labelEx($model, getTranslatedString('Position on trailer for damage')); ?>
+            </td><td>
+                <?php echo $form->dropDownList($model, 'Damageposition', $damagepos); ?>
+                <?php //echo $form->hiddenField($model,'Damageposition',array('type'=>"hidden",'size'=>2,'maxlength'=>60 ,'value'=>'Höger sida (Right side)')); ?>
+                <?php echo $form->error($model, 'Damageposition'); ?>
+
+            </td>	  
+            <td>
+                <?php echo $form->labelEx($model, getTranslatedString('Driver caused damage')); ?>
+            </td><td>
+
+                <?php echo $form->dropDownList($model, 'drivercauseddamage', $drivercauseddamageList); ?>
+            </td>	
+        </tr>
+        <tr>
+
+            <td>
+                <?php echo $form->labelEx($model, getTranslatedString('Damage Reported')); ?>
+            </td>
+            <td>
+                <?php echo $form->dropDownList($model, 'reportdamage', $reportdamage); ?>
+            </td>
+
+            <td><?php echo $form->labelEx($model, getTranslatedString('Ticket Status')); ?></td>
+            <td><?php
+                $ticketstatus2 = array();
+                foreach ($ticketstatus as $status) {
+
+                    if ($status == 'Open' || $status == 'Closed') {
+                        $ticketstatus2[$status] = $status;
+                    }
+                }
+                echo $form->dropDownList($model, 'ticketstatus', $ticketstatus2);
+                ?></td>
+        </tr>
+        <tr>
+            <td>
+                <?php echo $form->labelEx($model, getTranslatedString('Upload Pictures')); ?>
+            </td>
+
+            <td>
+                <?php
+                echo $form->fileField($model, 'image');
+                ?>
+                <?php echo $form->error($model, 'image'); ?>
+                <span id="addmorepic"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/001_01.png" title="Add More Pictures" /></span>
+                <p id="morepic"></p> 
+            </td>
+            <td>
+                <?php echo $form->labelEx($model, getTranslatedString('Create Date')); ?>
+            </td>
+            <td>
+                <?php echo date('Y-m-d'); ?>
+                <?php echo $form->hiddenField($model, 'TroubleTicketType', array('type' => "hidden", 'size' => 2, 'maxlength' => 2, 'value' => 'survey')); ?>
+            </td>
+        </tr>
+    </table>
+    <?php echo CHtml::submitButton(getTranslatedString('Submit'), array('id' => 'submit', 'name' => 'submit')); ?>
+    <?php echo CHtml::endForm(); ?>
+    <?php $this->endWidget(); ?>
+
+    <script>
+        var data = "<div><input type='file' name='Troubleticket[image";
+        var data2 = "]' id='file";
+        var data3 = "' />&nbsp;<span style='cursor:pointer'><img src='<?php echo Yii::app()->request->baseUrl; ?>/images/buttons_21.png' title='Remove Picurest'  onclick='removefiles(this)'/></span></div>";
+        $("#addmorepic").click(function() {
+            var counter = $('input[type=file]').length;
+            $('#morepic').append(data + counter + data2 + counter + data3);
+        });
+
+        function removefiles(obj)
+        {
+            $(obj).parents("div:first").remove();
+
+        }
+
+    </script>
 
--- applications/clab/trailer-app-portal/protected/views/troubleticket/surveylist.php
+++ applications/clab/trailer-app-portal/protected/views/troubleticket/surveylist.php
@@ -37,7 +37,7 @@ foreach ($month as $key => $val) {
 }
 
 foreach ($Assets as $key => $val) {
-    if ($TR==$val)
+    if ($TR == $val)
         $TID.="<option value=\"" . $key . "\" selected=\"selected\">" . $val . "</option>";
     else
         $TID.="<option value=\"" . $key . "\">" . $val . "</option>";
@@ -45,9 +45,9 @@ foreach ($Assets as $key => $val) {
 ?>
 <div id="wrap">
     <?php if (Yii::app()->params['createTroubleTicket']) { ?>
-    <div style="float:right; width:208px" class="button">
-        <a href="index.php?r=troubleticket/survey/"><?php echo getTranslatedString('Create new Trouble ticket'); ?></a>
-    </div>
+        <div style="float:right; width:208px" class="button">
+            <a href="index.php?r=troubleticket/survey/"><?php echo getTranslatedString('Create new Trouble ticket'); ?></a>
+        </div>
     <?php } ?>    
     <div class="toppanel">
         <table width="100%" border="0" cellspacing="0" cellpadding="1">
@@ -63,20 +63,20 @@ foreach ($Assets as $key => $val) {
                 </td>
                 <td valign="top"><table width="100%" border="0" cellspacing="1" cellpadding="1" style="background:#FFF; border:#CCC solid 1px; padding:5px;">
                         <tr>
-<?php
-if (!isset($currentasset)) {
-    $inopt = "disabled=disabled";
-    $damagechecked = "disabled=disabled";
-} else {
-    if ($currentasset['result']['assetstatus'] == 'Out-of-service') {
-        $damagechecked = "checked=checked";
-        $inopt = "";
-    } else {
-        $inopt = "checked=checked";
-        $damagechecked = "";
-    }
-}
-?>
+                            <?php
+                            if (!isset($currentasset)) {
+                                $inopt = "disabled=disabled";
+                                $damagechecked = "disabled=disabled";
+                            } else {
+                                if ($currentasset['result']['assetstatus'] == 'Out-of-service') {
+                                    $damagechecked = "checked=checked";
+                                    $inopt = "";
+                                } else {
+                                    $inopt = "checked=checked";
+                                    $damagechecked = "";
+                                }
+                            }
+                            ?>
                             <td><strong>Trailer</strong></td>
                             <td><select name='TID' id="trailer" onchange="getAjaxBaseRecord(this.value)"><?php echo $TID; ?></select></td>
                             <td><input type="radio" name="optration" <?php echo $inopt; ?> value="inoperation" id="inperation" onclick="getAjaxBaseAssetRecord(this.value)" value="inperation" style="margin-right:10px"><?php echo getTranslatedString('In operation'); ?>	
@@ -93,110 +93,116 @@ if (!isset($currentasset)) {
         <table id="table_id">
             <thead>
                 <tr>
-                    <th><?php echo getTranslatedString('ID');?></th>
-                    <th><?php echo getTranslatedString('Date');?></th>
-                    <th><?php echo getTranslatedString('Time');?></th>
-                    <th><?php echo getTranslatedString('Trailer ID');?></th>
-                    <th><?php echo getTranslatedString('Account');?></th>
-                    <th><?php echo getTranslatedString('Contact');?></th>
-                    <th><?php echo getTranslatedString('Place');?></th>
-                    <th><?php echo getTranslatedString('Plates');?></th>
-                    <th><?php echo getTranslatedString('Straps');?></th>
-                    <th><?php echo getTranslatedString('Damage Reported');?></th>
-                    <th><?php echo getTranslatedString('Type of damage');?></th>
-                    <th><?php echo getTranslatedString('Position on trailer');?></th>
-                    <th><?php echo getTranslatedString('Driver caused damage');?></th>
+                    <th><?php echo getTranslatedString('ID'); ?></th>
+                    <th><?php echo getTranslatedString('Date'); ?></th>
+                    <th><?php echo getTranslatedString('Time'); ?></th>
+                    <th><?php echo getTranslatedString('Trailer ID'); ?></th>
+                    <th><?php echo getTranslatedString('Account'); ?></th>
+                    <th><?php echo getTranslatedString('Contact'); ?></th>
+                    <th><?php echo getTranslatedString('Place'); ?></th>
+                    <th><?php echo getTranslatedString('Plates'); ?></th>
+                    <th><?php echo getTranslatedString('Straps'); ?></th>
+                    <th><?php echo getTranslatedString('Damage Reported'); ?></th>
+                    <th><?php echo getTranslatedString('Type of damage'); ?></th>
+                    <th><?php echo getTranslatedString('Position on trailer'); ?></th>
+                    <th><?php echo getTranslatedString('Driver caused damage'); ?></th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($result['result'] as $data) { ?>
-                <?php $date = date('y-m-d', strtotime($data['createdtime'])); ?>
-                <?php $time = date('h:i', strtotime($data['createdtime'])); ?>
-                <?php $viewdteails = '<span id=' . $data['id'] . '></span><a href="index.php?r=troubleticket/surveydetails/' . $data['id'] . '" onclick=waitprocess("' . $data['id'] . '")>' . $data['accountname'] . '</a>'; ?>
-                <?php $ticketNo = '<span id=' . $data['id'] . '-1></span><a href="index.php?r=troubleticket/surveydetails/' . $data['id'] . '" onclick=waitprocess("' . $data['id'] . '-1")>' . $data['date'] . '</a>';?>
-                <tr>
-                    <td><?php echo $data['ticket_no'];?></td>
-                    <td><?php echo $date;?></td>
-                    <td><?php echo $time;?></td>
-                    <td><?php echo $data['trailerid'];?></td>
-                    <td><?php echo $viewdteails;?></td>
-                    <td><?php echo $data['contactname'];?></td>
-                    <td><?php echo htmlentities($data['damagereportlocation'], ENT_QUOTES, "UTF-8");?></td>
-                    <td><?php echo htmlentities($data['plates'], ENT_QUOTES, "UTF-8");?></td>
-                    <td><?php echo htmlentities($data['straps'], ENT_QUOTES, "UTF-8");?></td>
-                    <td><?php echo $data['reportdamage'];?></td>
-                    <td><?php echo htmlentities($data['damagetype'], ENT_QUOTES, "UTF-8");?></td>
-                    <td><?php echo htmlentities($data['damageposition'], ENT_QUOTES, "UTF-8");?></td>
-                    <td><?php echo $data['drivercauseddamage'];?></td>
-                </tr>
+                    <?php $date = date('y-m-d', strtotime($data['createdtime'])); ?>
+                    <?php $time = date('h:i', strtotime($data['createdtime'])); ?>
+                    <?php $viewdteails = '<span id=' . $data['id'] . '></span><a href="index.php?r=troubleticket/surveydetails/' . $data['id'] . '" onclick=waitprocess("' . $data['id'] . '")>' . $data['accountname'] . '</a>'; ?>
+                    <?php $ticketNo = '<span id=' . $data['id'] . '-1></span><a href="index.php?r=troubleticket/surveydetails/' . $data['id'] . '" onclick=waitprocess("' . $data['id'] . '-1")>' . $data['date'] . '</a>'; ?>
+                    <tr>
+                        <td><?php echo $data['ticket_no']; ?></td>
+                        <td><?php echo $date; ?></td>
+                        <td><?php echo $time; ?></td>
+                        <td><?php echo $data['trailerid']; ?></td>
+                        <td><?php echo $viewdteails; ?></td>
+                        <td><?php echo $data['contactname']; ?></td>
+                        <td><?php echo htmlentities($data['damagereportlocation'], ENT_QUOTES, "UTF-8"); ?></td>
+                        <td><?php echo htmlentities($data['plates'], ENT_QUOTES, "UTF-8"); ?></td>
+                        <td><?php echo htmlentities($data['straps'], ENT_QUOTES, "UTF-8"); ?></td>
+                        <td><?php echo $data['reportdamage']; ?></td>
+                        <td><?php echo htmlentities($data['damagetype'], ENT_QUOTES, "UTF-8"); ?></td>
+                        <td><?php echo htmlentities($data['damageposition'], ENT_QUOTES, "UTF-8"); ?></td>
+                        <td><?php echo $data['drivercauseddamage']; ?></td>
+                    </tr>
                 <?php } ?>
             </tbody>
         </table>
     </div>
 </div>
-<script>
-    jQuery(document).ready(function(){
-        jQuery("#assetsmsg").show().delay(5000).fadeOut();
-        jQuery('#table_id').dataTable();
-    });
-    function getAjaxBaseAssetRecord(value)
-    {
+<script type="text/javascript">
+                    jQuery(document).ready(function() {
+                        jQuery("#assetsmsg").show().delay(5000).fadeOut();
+                        jQuery('#table_id').dataTable();
+                    });
+                    function getAjaxBaseAssetRecord(value)
+                    {
 
-        if(value=='damaged')
-        {
-            var tickettype=value;
-        }	
-        else
-        {
-            var tickettype='inoperation';
-        }
-        var trailer=$('#trailer').val();
-        $("#assetsmsg").addClass("waitprocess");
-        $('#assetsmsg').html('loading....  Please wait');
-        $.post('index.php?r=troubleticket/changeassets',{tickettype: tickettype,trailer:trailer},
-        function(data) 
-        {
-            $("#assetsmsg").removeClass("waitprocess");
-            $('#assetsmsg').html(data);
-        });
+                        if (value == 'damaged')
+                        {
+                            var tickettype = value;
+                        }
+                        else
+                        {
+                            var tickettype = 'inoperation';
+                        }
+                        var trailer = $('#trailer').val();
+                        $("#assetsmsg").addClass("waitprocess");
+                        $('#assetsmsg').html('loading....  Please wait');
+                        $.post('index.php?r=troubleticket/changeassets', {tickettype: tickettype, trailer: trailer},
+                        function(data)
+                        {
+                            $("#assetsmsg").removeClass("waitprocess");
+                            $('#assetsmsg').html(data);
+                        });
 
-    }
-    function getAjaxBaseRecord(value)
-    {
-        var year=$('#year').val();
-        var month=$('#month').val();
-        var reportdamage=$('#reportdamage').val();
-        var trailer=$('#trailer option:selected').text();
-        $("#process").addClass("waitprocess");	
-        $('#process').html('loading....  Please wait');
-        $.post('index.php?r=troubleticket/surveysearch',{year: year, month: month ,trailer:trailer, reportdamage:reportdamage},
-        function(data) 
-        {
-            $("#process").removeClass("waitprocess");
-            $('#wrap').html(data);
-        });
-	
-    }
+                    }
+                    function getAjaxBaseRecord(value)
+                    {
+                        var year = $('#year').val();
+                        var month = $('#month').val();
+                        var reportdamage = $('#reportdamage').val();
+                        var trailer = $('#trailer option:selected').text();
+                        $("#process").addClass("waitprocess");
+                        $('#process').html('loading....  Please wait');
+                        $.post('index.php?r=troubleticket/surveysearch', {year: year, month: month, trailer: trailer, reportdamage: reportdamage},
+                        function(data)
+                        {
+                            $("#process").removeClass("waitprocess");
+                            $('#wrap').html(data);
+                        });
+
+                    }
 
 
-    function waitprocess(id)
-    {
-        $("#"+id).addClass("waitprocessdetails");	
-        $('#'+id).html('Please wait...');
-    }
-    $('#year').val('<?php if (!empty($SYear)) {
-            echo $SYear;
-        } else {
-            echo date('Y');
-        } ?>');
-    $('#month').val('<?php if (!empty($SMonth)) {
-            echo $SMonth;
-        } else {
-            echo date('m');
-        } ?>');
-    $('#reportdamage').val('<?php if (!empty($SReportdamage)) {
-            echo $SReportdamage;
-        } else {
-            echo 'all';
-        } ?>');            
+                    function waitprocess(id)
+                    {
+                        $("#" + id).addClass("waitprocessdetails");
+                        $('#' + id).html('Please wait...');
+                    }
+                    $('#year').val('<?php
+                if (!empty($SYear)) {
+                    echo $SYear;
+                } else {
+                    echo date('Y');
+                }
+                ?>');
+                    $('#month').val('<?php
+                if (!empty($SMonth)) {
+                    echo $SMonth;
+                } else {
+                    echo date('m');
+                }
+                ?>');
+                    $('#reportdamage').val('<?php
+                if (!empty($SReportdamage)) {
+                    echo $SReportdamage;
+                } else {
+                    echo 'all';
+                }
+                ?>');
 </script>
