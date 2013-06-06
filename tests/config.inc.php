4c9eaed323d763e2c5ba95cf030e81889e811a32
Anshuk Kumar <gizur-ess-anshuk@gizur.com>
2013-06-06 10:13:37 +0530

    changed gc1-ireland for DocumentAttachment

--- tests/config.inc.php
+++ tests/config.inc.php
@@ -3,7 +3,7 @@
 class configuration {
 
     public function get() {
-        
+        /*
 	    return array(
 	       'url' => 'https://api.gizur.com/api/',
 	       'GIZURCLOUD_API_KEY' => 'GZCLD50694086B196F50694086B19E7',
@@ -14,18 +14,19 @@ class configuration {
 		    //'jonas.colmsjo@gizur.com' => '507d136b23699',
 	       )
 	    );
-        /*
+        */
 	    return array(
 	       'url' => 'https://api.gizur.com/api/',
 	       'GIZURCLOUD_API_KEY' => 'GZCLD50EE9D44BEBD450EE9D44BEC50',
 	       'GIZURCLOUD_SECRET_KEY' => '50ee9d44beb2d0.0165098250ee9d44beb591.45044222',
 	       'API_VERSION' => '0.1',
 	       'credentials' => array(
-		    'demo@gizur.com' => 'demo',
-		    //'jonas.colmsjo@gizur.com' => '507d136b23699',
-	       )
+		       'demo@gizur.com' => 'demo',
+		       //'jonas.colmsjo@gizur.com' => '507d136b23699',
+	       ),
+           'clientid' => 'demo'
 	    );
-        */
+        
     }
     
     public function getBikerPortalCredentials()
