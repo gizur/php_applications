4a2fb0ef2ffe4d6fb9a85b4b1921b96abd3c87d5
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-06-12 17:30:03 +0530

    Testing memcache

--- applications/s3-mem/index.php
+++ applications/s3-mem/index.php
@@ -4,15 +4,14 @@ $memcache_url = '10.58.226.192';
 $file = './img/sample.jpg';
 $memcache = new Memcache;
 
-if (!$memcache->connect($memcache_url, 11211)) {
+if ($memcache->connect($memcache_url, 11211)) {
     $start_time = time();
-    $arr = array();
     $name = uniqid() . ".jpg";
     if (file_exists($file)) {
-        $size = getimagesize($file, $arr);
-        $fp = fopen($file, 'rb');
+        $size = getimagesize($file);
+        $fp = file_get_contents($file);
         if ($size and $fp) {
-            $memcache->set($name, fpassthru($fp));
+            $memcache->set($name, $fp);
         }
     } else {
         echo "File not exists.";
