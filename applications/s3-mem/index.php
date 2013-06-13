26fdab89a7018ea341f75246a81261f6e5003de4
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-06-13 17:30:14 +0530

    S3 test

--- applications/s3-mem/index.php
+++ applications/s3-mem/index.php
@@ -3,10 +3,9 @@
 $memcache_url = '10.58.226.192';
 $file = './img/sample.jpg';
 $memcache = new Memcache;
-
+$name = uniqid() . ".jpg";
+$start_time = time();
 if ($memcache->connect($memcache_url, 11211)) {
-    $start_time = time();
-    $name = uniqid() . ".jpg";
     if (file_exists($file)) {
         $size = getimagesize($file);
         $fp = file_get_contents($file);
@@ -16,9 +15,31 @@ if ($memcache->connect($memcache_url, 11211)) {
     } else {
         echo "File not exists.";
     }
-    $end_time = time();
-
-    echo "<br/>Memcache took " . ($end_time - $start_time) . " Seconds";
 }
+$end_time = time();
+echo "<br/>Memcache took " . ($end_time - $start_time) . " Seconds";
+
+require_once __DIR__ . '../../lib/aws-php-sdk/sdk.class.php';
+$start_time = time();
+
+$_sThree = new AmazonS3();
+$responseSThree = $_sThree->create_object(
+    "gc3-image-test", $name, array(
+    'body' => file_get_contents($file),
+    'contentType' => 'plain/text',
+    'headers' => array(
+        'Cache-Control' => 'max-age',
+        'Content-Language' => 'en-US',
+        'Expires' =>
+        'Thu, 01 Dec 1994 16:00:00 GMT',
+    ))
+);
+
+if (!$responseSThree->isOK())
+    echo "S3 Transfer failed.";
+
+$end_time = time();
+echo "<br/>Memcache took " . ($end_time - $start_time) . " Seconds";
+
 
 ?>
