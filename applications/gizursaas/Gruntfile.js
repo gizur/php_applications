6b2357c46e12508cc0bc6c93648331e7c733e311
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-06-19 11:26:15 +0530

    Cleaned

--- applications/gizursaas/Gruntfile.js
+++ applications/gizursaas/Gruntfile.js
@@ -7,7 +7,6 @@ module.exports = function(grunt) {
         },
         // Plugin tasks
         //-------------
-
         doccoh: {
             src: ['*.js', 'app/**/*.js', 'config/*.js']
         },
@@ -23,6 +22,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-exec');
     grunt.loadNpmTasks('grunt-doccoh');
     grunt.loadNpmTasks('grunt-clean');
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Default task.
     grunt.registerTask('default', ['doccoh']);
