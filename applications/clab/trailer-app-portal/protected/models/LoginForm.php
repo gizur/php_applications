2c6e48da64cf1a6dec6d1c09624440d93b530583
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-07-20 14:42:21 +0530

    Trailer app portal validation added

--- applications/clab/trailer-app-portal/protected/models/LoginForm.php
+++ applications/clab/trailer-app-portal/protected/models/LoginForm.php
@@ -125,47 +125,53 @@ class LoginForm extends CFormModel
 
     function changepassword($oldpassword, $newpassword, $newpassword1)
     {
-        if ($newpassword != $newpassword1) {
-            $model = 'Authenticate';
-            // echo " Getting Picklist" . PHP_EOL;        
-            $params = array(
-                'Verb' => 'PUT',
-                'Model' => $model,
-                'Version' => Yii::app()->params->API_VERSION,
-                'Timestamp' => date("c"),
-                'KeyID' => Yii::app()->params->GIZURCLOUD_API_KEY,
-                'UniqueSalt' => uniqid()
-            );
-            // Sorg arguments
-            ksort($params);
-            $data = array('newpassword' => $newpassword);
-            // Generate string for sign
-            $string_to_sign = "";
-            foreach ($params as $k => $v)
-                $string_to_sign .= "{$k}{$v}";
+        if (!empty($oldpassword) &&
+            !empty($newpassword) &&
+            !empty($newpassword1)) {
+            if ($newpassword == $newpassword1) {
+                $model = 'Authenticate';
+                // echo " Getting Picklist" . PHP_EOL;        
+                $params = array(
+                    'Verb' => 'PUT',
+                    'Model' => $model,
+                    'Version' => Yii::app()->params->API_VERSION,
+                    'Timestamp' => date("c"),
+                    'KeyID' => Yii::app()->params->GIZURCLOUD_API_KEY,
+                    'UniqueSalt' => uniqid()
+                );
+                // Sorg arguments
+                ksort($params);
+                $data = array('newpassword' => $newpassword);
+                // Generate string for sign
+                $string_to_sign = "";
+                foreach ($params as $k => $v)
+                    $string_to_sign .= "{$k}{$v}";
 
-            // Generate signature
-            $signature = base64_encode(hash_hmac('SHA256', $string_to_sign, Yii::app()->params->GIZURCLOUD_SECRET_KEY, 1));
-            //login using each credentials
-            $response['result'] = array();
-            $rest = new RESTClient();
-            $rest->format('json');
-            $rest->set_header('X_USERNAME', Yii::app()->session['username']);
-            $rest->set_header('X_PASSWORD', $oldpassword);
-            $rest->set_header('X_TIMESTAMP', $params['Timestamp']);
-            $rest->set_header('X_UNIQUE_SALT', $params['UniqueSalt']);
-            $rest->set_header('X_SIGNATURE', $signature);
-            $rest->set_header('X_GIZURCLOUD_API_KEY', Yii::app()->params->GIZURCLOUD_API_KEY);
-            $response = $rest->put(Yii::app()->params->URL . $model . "/changepw", $data);
-            $response = json_decode($response, true);
-            //check if response is valid
-            if ($response['error']['success'] == true) {
-                echo Yii::app()->user->setFlash('success', "Your password has been changed successfully.");
+                // Generate signature
+                $signature = base64_encode(hash_hmac('SHA256', $string_to_sign, Yii::app()->params->GIZURCLOUD_SECRET_KEY, 1));
+                //login using each credentials
+                $response['result'] = array();
+                $rest = new RESTClient();
+                $rest->format('json');
+                $rest->set_header('X_USERNAME', Yii::app()->session['username']);
+                $rest->set_header('X_PASSWORD', $oldpassword);
+                $rest->set_header('X_TIMESTAMP', $params['Timestamp']);
+                $rest->set_header('X_UNIQUE_SALT', $params['UniqueSalt']);
+                $rest->set_header('X_SIGNATURE', $signature);
+                $rest->set_header('X_GIZURCLOUD_API_KEY', Yii::app()->params->GIZURCLOUD_API_KEY);
+                $response = $rest->put(Yii::app()->params->URL . $model . "/changepw", $data);
+                $response = json_decode($response, true);
+                //check if response is valid
+                if ($response['error']['success'] == true) {
+                    echo Yii::app()->user->setFlash('success', "Your password has been changed successfully.");
+                } else {
+                    echo Yii::app()->user->setFlash('error', $response['error']['message']);
+                }
             } else {
-                echo Yii::app()->user->setFlash('error', $response['error']['message']);
+                echo Yii::app()->user->setFlash('error', "Both new passwords do not match.");
             }
         } else {
-            echo Yii::app()->user->setFlash('error', "Both new passwords do not match.");
+            echo Yii::app()->user->setFlash('error', "All fields are mandatory.");
         }
     }
 
