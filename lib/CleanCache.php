a323ac2eb4770af33e6c4c46f95f29f1b88f5d7b
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-08-03 15:14:07 +0530

    cache clean class added

--- lib/CleanCache.php
+++ lib/CleanCache.php
@@ -1,11 +1,10 @@
 <?php
 
 /**
- * Yii Controller to clean cache
+ * Class to clean cache
  * 
  * PHP version 5
- *
- * @category   Controller
+ * 
  * @package    GizurCloud
  * @subpackage Controller
  * @author     Prabhat Khera <prabhat.khera@essindia.co.in>
@@ -14,39 +13,26 @@
  * @link       http://api.gizur.com/api/index.php
  * 
  * */
-
 /*
  * Including Amazon classes
  */
 
-spl_autoload_unregister(array('YiiBase', 'autoload'));
-Yii::import('application.vendors.*');
 require_once 'aws-php-sdk/sdk.class.php';
-spl_autoload_register(array('YiiBase', 'autoload'));
 
-class CacheCleanController extends Controller
+class CacheClean
 {
-
-    /**
-     * Filters executable action on permission basis
-     * 
-     * @return array action filters
-     */
-    public function filters()
+    public function __construct()
     {
-        return array();
+        
     }
 
-    public function beforeAction($action)
-    {
-            
-    }
-    
+
     public function actionRemove()
     {
         $ec2 = new AmazonEC2();
         $ec2->set_region(constant("AmazonEC2::" . Yii::app()->params->awsDynamoDBRegion));
-        
+
         print_r($ec2->describe_instances());
     }
+
 }
