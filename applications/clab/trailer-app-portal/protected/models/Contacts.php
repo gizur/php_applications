d792abd686896026270c771773377a2b2f850711
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2014-02-05 12:47:53 +0530

    added findAll function on conatct model for fetching all contact list

--- applications/clab/trailer-app-portal/protected/models/Contacts.php
+++ applications/clab/trailer-app-portal/protected/models/Contacts.php
@@ -1,17 +1,13 @@
 <?php
 
-/* Assets class
+/* Contacts class
  * 
  */
-
-class Assets extends CFormModel {
-    
-    
+class Contacts extends CFormModel {
     /* Funcation Name:- findAll
      * Description:- Get all contacts form Vtiger using rest api.
      * Return Type: Json
-     */
-    
+     */ 
     function findAll($module, $actionType = NULL, $filter = NULL) {
         $params = array(
             'Verb' => 'GET',
@@ -21,15 +17,12 @@ class Assets extends CFormModel {
             'KeyID' => Yii::app()->params->GIZURCLOUD_API_KEY,
             'UniqueSalt' => uniqid()
         );
-
         // Sorg arguments
         ksort($params);
-
         // Generate string for sign
         $string_to_sign = "";
         foreach ($params as $k => $v)
             $string_to_sign .= "{$k}{$v}";
-
         // Generate signature
         $signature = base64_encode(hash_hmac('SHA256', $string_to_sign, Yii::app()->params->GIZURCLOUD_SECRET_KEY, 1));
         //login using each credentials
@@ -40,7 +33,6 @@ class Assets extends CFormModel {
             }
             $searchString = '/' . $actionType . '/s?searchString=' . base64_encode($filter);
         }
-
         //foreach($this->credentials as $username => $password){            
         $rest = new RESTClient();
         $rest->format('json');
@@ -54,7 +46,6 @@ class Assets extends CFormModel {
         return $result = json_decode($response, true);
     }
 
-    
 }
 
 ?>
