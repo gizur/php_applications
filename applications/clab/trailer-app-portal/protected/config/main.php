8fd4c5ef3ba6b6e6b725412c4a3fce4782292036
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2014-02-21 09:37:12 +0530

    activated assets and contacts tab for clab

--- applications/clab/trailer-app-portal/protected/config/main.php
+++ applications/clab/trailer-app-portal/protected/config/main.php
@@ -6,7 +6,7 @@
 // CWebApplication properties can be configured here.
 return array(
     'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
-    'name' => 'Coop Trailer App',
+    'name' => 'Clabtraining Trailer App',
     // preloading 'log' component
     'preload' => array('log'),
     // autoloading model and component classes
