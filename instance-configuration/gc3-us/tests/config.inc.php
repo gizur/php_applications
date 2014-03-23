229f523a6ae477a7c285c00ef3459ca97e349199
Cloud3 <cloud3@gizur.com>
2014-03-23 12:53:10 +0100

    new demo first commit

---
+++ instance-configuration/gc3-us/tests/config.inc.php
@@ -0,0 +1,33 @@
+<?php
+
+class configuration
+{
+
+    public function get()
+    {
+        return array(
+            'url' => 'http://phpapplications-env-sixmtjkbzs.elasticbeanstalk.com/api/',
+            'GIZURCLOUD_API_KEY' => 'GZCLD51A309109FD3551A309109FE26',
+            'GIZURCLOUD_SECRET_KEY' => "51a309109fca42.1554128151a309109fcb20.84498075",
+            'API_VERSION' => '0.1',
+            'credentials' => array(
+                'portal_user@gizur.com' => "2hxrftmd",
+                //'mobile_user@gizur.com' => 'ivry34aq',
+            ),
+            'clientid' => 'clab'
+        );
+    }
+
+    public function getBikerPortalCredentials()
+    {
+        return array(
+            'url' => 'http://phpapplications-env-sixmtjkbzs.elasticbeanstalk.com/applications/cikab',
+            'credentials' => array(
+                array('user_name' => 'gizur-ess-prabhat@gizur.com',
+                    'user_password' => 'essindia', 'id' => 2),
+            ),
+            'version' => '5.4.0'
+        );
+    }
+
+}
