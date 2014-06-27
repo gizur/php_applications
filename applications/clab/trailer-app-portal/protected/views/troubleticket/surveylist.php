b64799d33c91c823f5c413a762aa8be13a17d725
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2014-06-27 22:00:55 +0530

    set limit 500

--- applications/clab/trailer-app-portal/protected/views/troubleticket/surveylist.php
+++ applications/clab/trailer-app-portal/protected/views/troubleticket/surveylist.php
@@ -126,7 +126,7 @@ foreach ($rm as $key => $val) {
     </div>
 </div>
 <script type="text/javascript">
-   var maxdataLimit =100;
+   var maxdataLimit =500;
     jQuery(document).ready(function() {
         jQuery("#assetsmsg").show().delay(5000).fadeOut();
         window.dt = jQuery('#table_id').dataTable({
