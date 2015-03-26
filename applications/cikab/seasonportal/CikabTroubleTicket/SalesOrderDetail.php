1df4c3451988596b1b49a8d4689b10a00352a158
gizur-ess-prabhat <gizur-ess-prabhat@gizur.com>
2015-03-26 10:56:43 +0530

    changes in salesorderdetail

--- applications/cikab/seasonportal/CikabTroubleTicket/SalesOrderDetail.php
+++ applications/cikab/seasonportal/CikabTroubleTicket/SalesOrderDetail.php
@@ -1,5 +1,5 @@
 <?php
-echo '<table><tr><td><input class="crmbutton small cancel" type="button" value="'.getTranslatedString('LBL_BACK_BUTTON').'" onclick="window.history.back();"/></td></tr></table>';
+echo '<table><tr><td><input class="crmbutton small cancel" type="button" value="'.getTranslatedString('LBL_BACK_BUTTON').'" onclick="back();"/></td></tr></table>';
 if (isset($_REQUEST['salesorder_no']) && !empty($_REQUEST['salesorder_no']))
     echo str_replace('${number}', $_REQUEST['salesorder_no'], getTranslatedString('LBL_SUCCESS_IN_SAVING_SALESORDER'));
 else
@@ -12,3 +12,13 @@ else
 </td>
 </tr>
 </table>
+<script type="text/javascript">
+   function back()
+{
+        var protocol=window.location.protocol;
+        var hostname=window.location.hostname;
+        var path=window.location.pathname;
+        var url=protocol+"//"+hostname+path+"?module=CikabTroubleTicket&action=index";
+        window.location.assign(url);
+}
+</script>
